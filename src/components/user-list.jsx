542a764b3e8de293d35c7b1b3e9179bdf67cfe25
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-03 16:09:35 +0100

    :construction: Added UserList

---
+++ src/components/user-list.jsx
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+function UserList({ users }) {
+  return (
+    <ul>
+      {users.map(user => <li key={`user-list-${user.username}`}>{user.username}</li>)}
+    </ul>
+  );
+}
+
+UserList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({ username: PropTypes.string.isRequired })).isRequired
+};
+
+export default UserList;
