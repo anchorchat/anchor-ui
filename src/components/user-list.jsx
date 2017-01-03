f42d410307969ee7c2f8d222c880b2fb6be7a40c
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-03 17:08:18 +0100

    :lipstick: Added csjs to userlist

--- src/components/user-list.jsx
+++ src/components/user-list.jsx
@@ -1,15 +1,26 @@
 import React, { PropTypes } from 'react';
+import csjs from 'csjs';
+import withStyles from 'react-csjs';
 
-function UserList({ users }) {
+const styles = csjs`
+.ul {
+  padding-left: 0
+}
+.li {
+  list-style: none
+}`;
+
+function UserList({ users, classes }) {
   return (
-    <ul>
-      {users.map(user => <li key={`user-list-${user.username}`}>{user.username}</li>)}
+    <ul className={classes.ul}>
+      {users.map(user => (<li className={classes.li} key={`user-list-${user.username}`}>{user.username}</li>))}
     </ul>
   );
 }
 
 UserList.propTypes = {
-  users: PropTypes.arrayOf(PropTypes.shape({ username: PropTypes.string.isRequired })).isRequired
+  users: PropTypes.arrayOf(PropTypes.shape({ username: PropTypes.string.isRequired })).isRequired,
+  classes: React.PropTypes.objectOf(React.PropTypes.object).isRequired
 };
 
-export default UserList;
+export default withStyles(styles)(UserList);
