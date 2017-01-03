c0baebe70efb58fbeac52c4cdf0259384384a8da
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-03 17:26:58 +0100

    :lipstick: Added styles

--- src/components/message-input.jsx
+++ src/components/message-input.jsx
@@ -1,12 +1,20 @@
 import React, { PropTypes } from 'react';
+import csjs from 'csjs';
+import withStyles from 'react-csjs';
 
-function MessageInput({ onChange, value }) {
-  return <input onChange={onChange} value={value} type="text" />;
+const styles = csjs`
+.input:focus {
+  outline: 0
+}`;
+
+function MessageInput({ onChange, value, classes }) {
+  return <input className={classes.input} onChange={onChange} value={value} type="text" />;
 }
 
 MessageInput.propTypes = {
   onChange: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired,
+  classes: React.PropTypes.objectOf(React.PropTypes.object).isRequired
 };
 
-export default MessageInput;
+export default withStyles(styles)(MessageInput);
