08f549577f95c814ed95936dd44a543d933ce03a
IanCStewart <ian.c.stewart@icloud.com>
2017-01-04 18:33:19 +0100

    :lipstick: Profile card styling Added profile card styling file.

--- src/style/colors.js
+++ src/style/colors.js
@@ -2,7 +2,8 @@ const colors = {
   white: '#FEFEFE',
   lightGrey: '#F1F1F1',
   grey: '#DEDEDE',
-  black: '#151515'
+  black: '#151515',
+  green: '#54D38A'
 };
 
 export default colors;
--- src/style/inputs.js
+++ src/style/inputs.js
@@ -1,13 +1,12 @@
 const style = {
   messageInput: {
-    display: 'flex',
     appearance: 'none',
     border: '0',
     width: '100%',
     height: '48px',
-    'padding-left': '10px',
-    'box-sizing': 'border-box',
-    'font-size': '16px',
+    paddingLeft: '10px',
+    boxSizing: 'border-box',
+    fontSize: '16px',
     '&:focus': {
       outline: 'none'
     }
