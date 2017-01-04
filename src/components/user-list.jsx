6a66356f24c2c8d2d6c1c9e5d60ae6d91a21f0c4
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-04 09:46:47 +0100

    :heavy_minus_sign: Removed csjs

--- src/components/user-list.jsx
+++ src/components/user-list.jsx
@@ -1,26 +1,15 @@
 import React, { PropTypes } from 'react';
-import csjs from 'csjs';
-import withStyles from 'react-csjs';
 
-const styles = csjs`
-.ul {
-  padding-left: 0
-}
-.li {
-  list-style: none
-}`;
-
-function UserList({ users, classes }) {
+function UserList({ users }) {
   return (
-    <ul className={classes.ul}>
-      {users.map(user => (<li className={classes.li} key={`user-list-${user.username}`}>{user.username}</li>))}
+    <ul>
+      {users.map(user => (<li key={`user-list-${user.username}`}>{user.username}</li>))}
     </ul>
   );
 }
 
 UserList.propTypes = {
   users: PropTypes.arrayOf(PropTypes.shape({ username: PropTypes.string.isRequired })).isRequired,
-  classes: React.PropTypes.objectOf(React.PropTypes.object).isRequired
 };
 
-export default withStyles(styles)(UserList);
+export default UserList;
