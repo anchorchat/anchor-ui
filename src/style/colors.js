eb474652322995c83235f625b95b62eab3870f1e
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-05 12:14:52 +0100

    :lipstick: Added more colors

--- src/style/colors.js
+++ src/style/colors.js
@@ -1,9 +1,11 @@
 const colors = {
-  white: '#FEFEFE',
   lightGrey: '#F1F1F1',
   grey: '#DEDEDE',
   black: '#151515',
-  green: '#54D38A'
+  white: '#FEFEFE',
+  green: '#54D38A',
+  primaryText: '#B4BEC6',
+  secondaryText: '#757F85'
 };
 
 export default colors;
