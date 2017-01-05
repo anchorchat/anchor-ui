07df63448d6df5ab09989c35c7632041d038d3d1
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-05 10:38:02 +0100

    :art: Changed prop name

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 import moment from 'moment';
 
-function Message({ message, momentFormat }) {
-  const format = momentFormat || 'HH:mm';
+function Message({ message, timeFormat }) {
+  const format = timeFormat || 'HH:mm';
 
   return (
     <section>
@@ -21,7 +21,7 @@ Message.propTypes = {
     createdAt: PropTypes.instanceOf(Date).isRequired,
     username: PropTypes.string.isRequired,
   }).isRequired,
-  momentFormat: PropTypes.string
+  timeFormat: PropTypes.string
 };
 
 export default Message;
