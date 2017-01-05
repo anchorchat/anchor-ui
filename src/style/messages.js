f0056dd86704bf292f3b8e4708328ac9d17d7811
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-05 12:15:36 +0100

    :lipstick: Finished basic message style

--- src/style/messages.js
+++ src/style/messages.js
@@ -5,31 +5,43 @@ const style = {
     maxWidth: '75%',
     padding: '7.5px',
     boxSizing: 'border-box',
-    backgroundColor: colors.green,
+    backgroundColor: colors.white,
     borderRadius: '3px',
-    color: colors.white,
+    color: colors.primaryText,
     position: 'relative',
     marginBottom: '30px',
     float: 'left'
+  },
+  myMessage: {
+    float: 'right',
+    backgroundColor: colors.green,
+    color: 'white',
+    '& $messageHeader': {
+      textAlign: 'right',
+      color: colors.white
+    },
+    '& $messageTime': {
+      color: colors.primaryText
+    }
   },
   messageHeader: {
     fontWeight: 'bold',
     fontSize: '12px',
     lineHeight: '12px',
-    marginBottom: '5px'
+    marginBottom: '5px',
+    color: colors.secondaryText
   },
   messageBody: {
     margin: '0',
     fontSize: '14px',
-    lineHeight: '14px'
+    lineHeight: '16px'
   },
   messageTime: {
     fontSize: '10px',
     lineHeight: '10px',
     position: 'absolute',
     bottom: '-15px',
-    right: '7.5px',
-    color: colors.grey
+    right: '7.5px'
   }
 };
 
