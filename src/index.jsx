6f868fdf6f07d52e7378ffbc4d9b8029cae45d3f
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-05 10:31:16 +0100

    :art: Added exports for Icons

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,43 @@
+// Components
+import ChannelHeader from './components/channel-header';
 import List from './components/list';
 import ListItem from './components/list-item';
-import MessageInput from './components/message-input';
+import Message from './components/message';
 import Messages from './components/messages';
-import ChannelHeader from './components/channel-header';
+import MessageInput from './components/message-input';
 import MyProfileCard from './components/my-profile-card';
+// Icons
+import IconAddImage from './icons/icon-add-image';
+import IconChannel from './icons/icon-channel';
+import IconChannels from './icons/icon-channels';
+import IconClock from './icons/icon-clock';
+import IconConversation from './icons/icon-conversation';
+import IconEdit from './icons/icon-edit';
+import IconEmoji from './icons/icon-emoji';
+import IconMenu from './icons/icon-menu';
+import IconPeople from './icons/icon-people';
+import IconPerson from './icons/icon-person';
+import IconSend from './icons/icon-send';
 
-export { List, ListItem, Messages, MessageInput, ChannelHeader, MyProfileCard };
+export {
+  // Components
+  ChannelHeader,
+  List,
+  ListItem,
+  Message,
+  Messages,
+  MessageInput,
+  MyProfileCard,
+  // Icons
+  IconAddImage,
+  IconChannel,
+  IconChannels,
+  IconClock,
+  IconConversation,
+  IconEdit,
+  IconEmoji,
+  IconMenu,
+  IconPeople,
+  IconPerson,
+  IconSend
+};
