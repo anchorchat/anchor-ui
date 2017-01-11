d86040afbfe5cdd4509da0ad5d27fb6e846c0f7d
IanCStewart <ian.c.stewart@icloud.com>
2017-01-11 12:16:33 +0100

    :white_check_mark: Button test Added button component test.

--- test/button.test.js
+++ test/button.test.js
@@ -3,14 +3,34 @@
 const React = require('react');
 const enzyme = require('enzyme');
 const chai = require('chai');
+const sinon = require('sinon');
 const Button = require('../dist/components/button');
 
 describe('<Button />', () => {
   const children = <p>children</p>;
 
-  it('renders children', () => {
+  it('should contain children when passed in', () => {
     const wrapper = enzyme.shallow(<Button onClick={() => {}}>{children}</Button>);
 
-    chai.assert.ok(wrapper.contains(children), 'should contain children');
+    chai.expect(wrapper.contains(children)).to.equal(true);
+  });
+
+  it('should simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = enzyme.shallow(<Button onClick={onButtonClick}>{children}</Button>);
+
+    wrapper.find('Button').simulate('click');
+    chai.expect(onButtonClick.calledOnce).to.equal(true);
+  });
+
+  it('allows us to overide styles', () => {
+    const wrapper = enzyme.shallow(<Button onClick={() => {}}>{children}</Button>);
+    const style = {
+      padding: '5px'
+    };
+
+    chai.expect(wrapper.props().style).to.equal(undefined);
+    wrapper.setProps({ style });
+    chai.expect(wrapper.props().style).to.equal(style);
   });
 });
