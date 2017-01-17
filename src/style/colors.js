5e464982488ce456d707859cefa87fdf89c51876
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-17 14:10:13 +0100

    :lipstick: Added colors for alert

--- src/style/colors.js
+++ src/style/colors.js
@@ -8,7 +8,13 @@ const colors = {
   secondaryText: '#B4BEC6',
   background: '#F2F7F7',
   icons: '#C4C4C4',
-  boxShadow: 'rgba(117, 127, 133, 0.23)'
+  boxShadow: 'rgba(117, 127, 133, 0.23)',
+  alert: {
+    success: '#DDF3D5',
+    error: '#EDC8C5',
+    warning: '#F8F4D5',
+    info: '#CDE8F5'
+  }
 };
 
 export default colors;
