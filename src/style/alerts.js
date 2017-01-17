c4191999f7e6c0d4a5087e30bfbe91979b241c00
Sjaak Luthart <mail@sjaakluthart.nl>
2017-01-17 14:10:50 +0100

    :lipstick: Added alert style

---
+++ src/style/alerts.js
@@ -0,0 +1,55 @@
+import colors from './colors';
+import darken from '../internal/darken';
+
+const styleSheet = {
+  alert: {
+    borderRadius: '3px',
+    boxSizing: 'border-box',
+    fontSize: '16px',
+    lineHeight: '20px',
+    padding: '14px 10px',
+    position: 'relative',
+    width: '300px'
+  },
+  icon: {
+    height: '24px',
+    position: 'absolute',
+    top: '13px',
+    width: '24px'
+  },
+  text: {
+    margin: '0',
+    paddingLeft: '32px',
+    paddingRight: '42px',
+    '&:first-letter': {
+      textTransform: 'capitalize'
+    }
+  },
+  button: {
+    position: 'absolute',
+    top: '5px',
+    right: '5px'
+  },
+  success: {
+    backgroundColor: colors.alert.success,
+    border: `1px solid ${darken(colors.alert.success, 0.1)}`,
+    color: darken(colors.alert.success, 0.65)
+  },
+  error: {
+    backgroundColor: colors.alert.error,
+    border: `1px solid ${darken(colors.alert.error, 0.1)}`,
+    color: darken(colors.alert.error, 0.65)
+  },
+  warning: {
+    backgroundColor: colors.alert.warning,
+    border: `1px solid ${darken(colors.alert.warning, 0.1)}`,
+    color: darken(colors.alert.warning, 0.65)
+  },
+  info: {
+    backgroundColor: colors.alert.info,
+    border: `1px solid ${darken(colors.alert.info, 0.1)}`,
+    color: darken(colors.alert.info, 0.65)
+  }
+};
+
+export default styleSheet;
