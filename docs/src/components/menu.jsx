8838468415a9e58718acfda449829fbdd61811b9
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-07 19:10:26 +0100

    :lipstick: Changed to List and ListItems

--- docs/src/components/menu.jsx
+++ docs/src/components/menu.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { List, ListItem } from 'anchor-ui';
 
 function Menu() {
   return (
     <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/alert">Alert</Link></li>
-        <li><Link to="/app-header">AppHeader</Link></li>
-        <li><Link to="/avatar">Avatar</Link></li>
-        <li><Link to="/badge">Badge</Link></li>
-        <li><Link to="/button">Button</Link></li>
-        <li><Link to="/channel-header">ChannelHeader</Link></li>
-        <li><Link to="/dialog">Dialog</Link></li>
-        <li><Link to="/empty-state">EmptyState</Link></li>
-        <li><Link to="/input">Input</Link></li>
-        <li><Link to="/list">List</Link></li>
-        <li><Link to="/list-item">ListItem</Link></li>
-        <li><Link to="/loader">Loader</Link></li>
-        <li><Link to="/message">Message</Link></li>
-        <li><Link to="/message-input">MessageInput</Link></li>
-        <li><Link to="/profile-card">ProfileCard</Link></li>
-      </ul>
+      <List>
+        <ListItem primaryText={<Link to="/">Home</Link>} />
+        <ListItem primaryText={<Link to="/alert">Alert</Link>} />
+        <ListItem primaryText={<Link to="/app-header">AppHeader</Link>} />
+        <ListItem primaryText={<Link to="/avatar">Avatar</Link>} />
+        <ListItem primaryText={<Link to="/badge">Badge</Link>} />
+        <ListItem primaryText={<Link to="/button">Button</Link>} />
+        <ListItem primaryText={<Link to="/channel-header">ChannelHeader</Link>} />
+        <ListItem primaryText={<Link to="/dialog">Dialog</Link>} />
+        <ListItem primaryText={<Link to="/empty-state">EmptyState</Link>} />
+        <ListItem primaryText={<Link to="/input">Input</Link>} />
+        <ListItem primaryText={<Link to="/list">List</Link>} />
+        <ListItem primaryText={<Link to="/list-item">ListItem</Link>} />
+        <ListItem primaryText={<Link to="/loader">Loader</Link>} />
+        <ListItem primaryText={<Link to="/message">Message</Link>} />
+        <ListItem primaryText={<Link to="/message-input">MessageInput</Link>} />
+        <ListItem primaryText={<Link to="/profile-card">ProfileCard</Link>} />
+      </List>
     </nav>
   );
 }
