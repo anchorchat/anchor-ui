48a568a19189223fdc2d1133ca4758ebec418eea
MrBamBam <mitchelvanbever@hotmail.com>
2017-02-08 10:32:16 +0100

    added more input examples

--- docs/src/components/input.jsx
+++ docs/src/components/input.jsx
@@ -16,7 +16,27 @@ function InputDoc() {
       </section>
       <section>
         <h1>Examples</h1>
-        <Input />
+        <Input
+          onChange={() => {}}
+          value="Start typing..."
+          type="text"
+          label="Text example"
+          name="example"
+        />
+        <Input
+          onChange={() => {}}
+          value="20"
+          type="number"
+          label="Number example"
+          name="example"
+        />
+        <Input
+          onChange={() => {}}
+          value=""
+          type="date"
+          label="Date example"
+          name="example"
+        />
       </section>
       <Props props={props} />
     </article>
