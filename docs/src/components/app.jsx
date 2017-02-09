95486b2296f62791f42ceb6099e7ddb74c95ada3
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-09 19:11:25 +0100

    :lipstick: Added github icon

--- docs/src/components/app.jsx
+++ docs/src/components/app.jsx
@@ -1,22 +1,34 @@
 import React, { PropTypes } from 'react';
-import { AppHeader } from 'anchor-ui';
+import { AppHeader, Button } from 'anchor-ui';
 import Menu from './menu';
 import logo from '../assets/images/logo.svg';
+import github from '../assets/images/github.svg';
 
 function App({ children }) {
   return (
     <main className="app">
       <AppHeader
-        text={
+        text="Anchor UI"
+        icon={
+          <a
+            href="https://anchor.chat"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} alt="Anchor Chat" />
+          </a>
+        }
+        rightButton={
           <a
             href="https://github.com/anchorchat/anchor-ui"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Anchor UI
+            <Button onClick={() => {}} iconButton>
+              <img className="github" src={github} alt="Github" />
+            </Button>
           </a>
         }
-        icon={<img src={logo} alt="Anchor Chat" />}
       />
       <article>
         <Menu />
