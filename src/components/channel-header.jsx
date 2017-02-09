0487a8c475bcf9494a08880890a63f923c64562a
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-09 16:39:57 +0100

    :hammer: Ported ChannelHeader to Radium

--- src/components/channel-header.jsx
+++ src/components/channel-header.jsx
@@ -1,100 +1,70 @@
-import React, { Component, PropTypes } from 'react';
-import injectSheet from 'react-jss';
-import shallowEqual from 'recompose/shallowEqual';
-import channelHeaderStyleSheet from '../style/channel-header';
-import getClassNames from '../internal/get-class-names';
+import React, { PropTypes } from 'react';
+import Radium from 'radium';
+import pure from 'recompose/pure';
+import styles from '../style/channel-header';
+import combineStyles from '../internal/combine-styles';
 
 /**
  * Channel header styling
  */
-class ChannelHeader extends Component {
-  static propTypes = {
-    /**
-     * Content of the header
-     */
-    name: PropTypes.node.isRequired,
-    /**
-     * Right-hand side placed button
-     */
-    rightButton: PropTypes.node,
-    /**
-     * Left-hand side placed button
-     */
-    leftButton: PropTypes.node,
-    sheet: PropTypes.shape({
-      classes: PropTypes.shape({
-        header: PropTypes.string.isRequired,
-        headerText: PropTypes.string.isRequired,
-        buttonRight: PropTypes.string.isRequired,
-        buttonLeft: PropTypes.string.isRequired
-      }).isRequired
-    }).isRequired,
-    /**
-     * Override the styles of the root element
-     */
-    style: PropTypes.instanceOf(Object),
-    /**
-     * Override the styles of the text element
-     */
-    headerTextStyle: PropTypes.instanceOf(Object),
-    /**
-     * Override the styles of the rightButton element
-     */
-    rightButtonStyle: PropTypes.instanceOf(Object),
-    /**
-     * Override the styles of the leftButton element
-     */
-    leftButtonStyle: PropTypes.instanceOf(Object)
-  }
-
-  static defaultProps = {
-    style: {},
-    headerTextStyle: {},
-    rightButtonStyle: {},
-    leftButtonStyle: {},
-    rightButton: null,
-    leftButton: null
-  }
-
-  constructor(props) {
-    super(props);
-
-    const { sheet: { classes }, style, headerTextStyle, rightButtonStyle, leftButtonStyle } = props;
-
-    const headerClassName = getClassNames(classes, style, 'header', 'ChannelHeader');
-    const textClassName = getClassNames(classes, headerTextStyle, 'headerText', 'ChannelHeader');
-    const rightButtonClassName = getClassNames(classes, rightButtonStyle, 'buttonRight', 'ChannelHeader');
-    const leftButtonClassName = getClassNames(classes, leftButtonStyle, 'buttonLeft', 'ChannelHeader');
-
-    this.state = {
-      textClassName,
-      headerClassName,
-      rightButtonClassName,
-      leftButtonClassName
-    };
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return (!shallowEqual(this.props, nextProps));
-  }
-
-  render() {
-    const { name, rightButton, leftButton } = this.props;
-    const {
-      textClassName,
-      headerClassName,
-      rightButtonClassName,
-      leftButtonClassName
-    } = this.state;
-
-    return (
-      <header className={headerClassName}>
-        {leftButton ? <div className={leftButtonClassName}>{leftButton}</div> : null}
-        <h1 className={textClassName}>{name}</h1>
-        {rightButton ? <div className={rightButtonClassName}>{rightButton}</div> : null}
-      </header>
-    );
-  }
+function ChannelHeader(
+  { name, rightButton, leftButton, style, leftButtonStyle, textStyle, rightButtonStyle }
+) {
+  return (
+    <header style={combineStyles(styles.header, style)}>
+      {
+        leftButton
+        ? <div style={combineStyles(styles.buttonLeft, leftButtonStyle)}>{leftButton}</div>
+        : null
+      }
+      <h1 style={combineStyles(styles.text, textStyle)}>{name}</h1>
+      {
+        rightButton
+        ? <div style={combineStyles(styles.buttonRight, rightButtonStyle)}>{rightButton}</div>
+        : null
+      }
+    </header>
+  );
 }
 
-export default injectSheet(channelHeaderStyleSheet)(ChannelHeader);
+ChannelHeader.propTypes = {
+  /**
+   * Content of the header
+   */
+  name: PropTypes.node.isRequired,
+  /**
+   * Right-hand side placed button
+   */
+  rightButton: PropTypes.node,
+  /**
+   * Left-hand side placed button
+   */
+  leftButton: PropTypes.node,
+  /**
+   * Override the styles of the root element
+   */
+  style: PropTypes.instanceOf(Object),
+  /**
+   * Override the styles of the text element
+   */
+  textStyle: PropTypes.instanceOf(Object),
+  /**
+   * Override the styles of the leftButton element
+   */
+  leftButtonStyle: PropTypes.instanceOf(Object),
+  /**
+   * Override the styles of the rightButton element
+   */
+  rightButtonStyle: PropTypes.instanceOf(Object)
+};
+
+ChannelHeader.defaultProps = {
+  style: {},
+  textStyle: {},
+  rightButtonStyle: {},
+  leftButtonStyle: {},
+  rightButton: null,
+  leftButton: null
+};
+
+export default pure(Radium(ChannelHeader));
