56b530a2c7cd9aa202eff10d4a216c50e83a91d2
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-10 10:24:04 +0100

    :bug: Always return a new object

--- src/internal/combine-styles.js
+++ src/internal/combine-styles.js
@@ -1,6 +1,9 @@
 function combineStyles(style, overrideStyle) {
   if (Object.keys(overrideStyle).length !== 0) {
-    return Object.assign(style, overrideStyle);
+    return {
+      ...style,
+      ...overrideStyle
+    };
   }
 
   return style;
