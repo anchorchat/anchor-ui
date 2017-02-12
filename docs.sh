465ec5cbd352fd58b7840d1d01ee36d68041db95
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-12 13:32:10 +0100

    :memo: Updated docs script

--- docs.sh
+++ docs.sh
@@ -2,7 +2,8 @@
 
 rm -rf components
 cp -r src components
-rm -rf components/icons components/internal components/with-theme components/style components/settings
+rm -rf components/internal components/with-theme components/style components/settings
+rm components/icons/index.js
 rm components/index.js components/url-regex.js
 npm run docs
 rm -rf components
