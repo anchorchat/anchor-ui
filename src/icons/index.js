4df4845f81c167a8f7756a22a0fee765a47264f8
Ian Stewart <IanCStewart@users.noreply.github.com>
2017-02-17 14:02:37 +0100

    Issue #188 (#194)
    * :bug: Added icons.
    Fixes #188. #188
    
    * :art: New and improved female icon.

--- src/icons/index.js
+++ src/icons/index.js
@@ -8,6 +8,8 @@ export IconClose from './icon-close';
 export IconConversation from './icon-conversation';
 export IconEdit from './icon-edit';
 export IconExit from './icon-exit';
+export IconGenderFemale from './icon-gender-female';
+export IconGenderMale from './icon-gender-male';
 export IconEmoji from './icon-emoji';
 export IconHammer from './icon-hammer';
 export IconHand from './icon-hand';
