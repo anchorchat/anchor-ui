9d5aef2b9d16212f3d6afcb3781a2b8860234f68
IanCStewart <ian.c.stewart@icloud.com>
2017-02-20 17:41:58 +0100

    :art: Added PopOver component.

---
+++ src/pop-over/index.jsx
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import Radium from 'radium';
+import pure from 'recompose/pure';
+import styles from '../style/pop-over';
+import combineStyles from '../internal/combine-styles';
+
+/**
+ * PopOver
+ */
+function PopOver({ children, style, open }) {
+  return open ? <ul style={combineStyles(styles.popOver, style)}>{children}</ul> : null;
+}
+
+PopOver.displayName = 'PopOver';
+
+PopOver.propTypes = {
+  /**
+   * PopOverItems to render
+   */
+  children: PropTypes.node.isRequired,
+  /**
+   * Override the styles of the root element
+   */
+  style: PropTypes.instanceOf(Object),
+  /**
+   * Boolean to check if it should be open
+   */
+  open: PropTypes.bool
+};
+
+PopOver.defaultProps = {
+  style: {},
+  open: false
+};
+
+export default pure(Radium(PopOver));
