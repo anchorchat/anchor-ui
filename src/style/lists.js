2be292239c6c29489cb0decb296d0d7583640d0c
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-21 11:05:24 +0100

    :sparkles: Added optional header to List

--- src/style/lists.js
+++ src/style/lists.js
@@ -8,6 +8,16 @@ const styleSheet = {
     margin: '0',
     paddingLeft: '0',
   },
+  listHeader: {
+    paddingTop: '10px',
+    paddingRight: '10px',
+    paddingBottom: '10px',
+    paddingLeft: '24px',
+    margin: '0',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    color: colors.primaryText
+  },
   listItem: {
     backgroundColor: colors.white,
     boxSizing: 'border-box',
