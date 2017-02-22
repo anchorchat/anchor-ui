7ab47947b65e61cb814ceeac4ef1b5fb1f2781a1
Sjaak Luthart <mail@sjaakluthart.nl>
2017-02-22 12:05:07 +0100

    :sparkles: Added optional header

--- src/pop-over/index.jsx
+++ src/pop-over/index.jsx
@@ -5,8 +5,17 @@ import styles from '../style/pop-over';
 import combineStyles from '../internal/combine-styles';
 
 /** Pop over useful for showing tooltips or menu options */
-function PopOver({ children, style, open }) {
-  return open ? <ul style={combineStyles(styles.popOver, style)}>{children}</ul> : null;
+function PopOver({ children, header, style, headerStyle, open }) {
+  if (!open) {
+    return null;
+  }
+
+  return (
+    <ul style={combineStyles(styles.popOver, style)}>
+      {header ? <h1 style={combineStyles(styles.header, headerStyle)}>{header}</h1> : null}
+      {children}
+    </ul>
+  );
 }
 
 PopOver.displayName = 'PopOver';
@@ -14,14 +23,20 @@ PopOver.displayName = 'PopOver';
 PopOver.propTypes = {
   /** Content of the PopOver */
   children: PropTypes.node.isRequired,
+  /** Optional header text */
+  header: PropTypes.node,
   /** Override the styles of the root element */
   style: PropTypes.instanceOf(Object),
   /** Boolean to check if it should be open */
-  open: PropTypes.bool
+  open: PropTypes.bool,
+  /** Override the styles of the header element */
+  headerStyle: PropTypes.instanceOf(Object)
 };
 
 PopOver.defaultProps = {
+  header: null,
   style: {},
+  headerStyle: {},
   open: false
 };
 
--- src/style/pop-over.js
+++ src/style/pop-over.js
@@ -12,6 +12,16 @@ const styleSheet = {
     margin: 0,
     paddingLeft: 0,
     overflow: 'hidden'
+  },
+  header: {
+    paddingTop: '10px',
+    paddingRight: '10px',
+    paddingBottom: '10px',
+    paddingLeft: '16px',
+    margin: '0',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    color: colors.primaryText
   }
 };
 
