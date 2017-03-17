48f2e6bba0bda06129ad2e2156fbc2cb95fa0808
Ian Stewart <IanCStewart@users.noreply.github.com>
2017-03-17 16:29:51 +0100

    Issue #297 (#300)
    * :hammer: Refactored getStyle.. to separate file.
    
    * :truck: Moved pop-over style file to own folder.
    
    * :bug: Added auto width to pop-over in select.
    Fixes #297. #297

---
+++ src/select/get-styles.js
@@ -0,0 +1,35 @@
+import colors from '../settings/colors';
+import combineStyles from '../internal/combine-styles';
+import styles from './styles';
+import darken from '../internal/darken';
+
+function icon(open, overrideStyle) {
+  let style = styles.icon;
+
+  if (open) {
+    style = combineStyles(style, { transform: 'rotate(180deg)' });
+  }
+
+  return combineStyles(style, overrideStyle);
+}
+
+function header(themeColor, overrideStyle) {
+  const color = themeColor || colors.theme;
+
+  const style = combineStyles(
+    styles.header,
+    {
+      backgroundColor: color,
+      ':hover': {
+        backgroundColor: darken(color, 0.05)
+      },
+      ':active': {
+        backgroundColor: darken(color, 0.15)
+      }
+    }
+  );
+
+  return combineStyles(style, overrideStyle);
+}
+
+export default { icon, header };
