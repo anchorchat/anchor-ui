647be3c9549717ff4cd3c7d93039ef96940964b9
Sjaak Luthart <mail@sjaakluthart.nl>
2017-03-20 11:35:25 +0100

    :hammer: Changed to function

--- src/tab/index.jsx
+++ src/tab/index.jsx
@@ -1,69 +1,46 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import Radium from 'radium';
 import pure from 'recompose/pure';
-import combineStyles from '../internal/combine-styles';
-import colors from '../settings/colors';
-import styles from './styles';
 import getStyles from './get-styles';
 
-function getStyle(themeColor, icon, selected, overrideStyle) {
-  let style = {};
-  const color = themeColor || colors.theme;
-
-  if (icon) {
-    style = combineStyles(style, { paddingLeft: '40px' });
-  }
-
-  if (selected) {
-    style = combineStyles(style, { color, paddingRight: '40px' });
-  }
-
-  return combineStyles(style, overrideStyle);
+function Tab({ onClick, icon, label, selected, style, iconStyle, labelStyle }, { color }) {
+  return (
+    <button style={getStyles.root(color, selected, style)} onClick={onClick}>
+      {icon ? <div style={getStyles.icon(iconStyle)}>{icon}</div> : null}
+      <span style={getStyles.label(labelStyle)}>{label}</span>
+    </button>
+  );
 }
 
-class Tab extends Component {
-  static displayName = 'Tab';
+Tab.displayName = 'Tab';
 
-  static propTypes = {
-    /** The Tab's icon */
-    icon: PropTypes.node,
-    /** The Tab's label */
-    label: PropTypes.node.isRequired,
-    /** Tab selected */
-    selected: PropTypes.bool,
-    /** Tab onClick function */
-    onClick: PropTypes.func.isRequired,
-    /** Override the styles of the root element */
-    style: PropTypes.instanceOf(Object),
-    /** Override the styles of the label element */
-    labelStyle: PropTypes.instanceOf(Object),
-    /** Override the styles of the icon element */
-    iconStyle: PropTypes.instanceOf(Object)
-  };
+Tab.propTypes = {
+  /** The Tab's icon */
+  icon: PropTypes.node,
+  /** The Tab's label */
+  label: PropTypes.node.isRequired,
+  /** Tab selected */
+  selected: PropTypes.bool,
+  /** Tab onClick function */
+  onClick: PropTypes.func.isRequired,
+  /** Override the styles of the root element */
+  style: PropTypes.instanceOf(Object),
+  /** Override the styles of the label element */
+  labelStyle: PropTypes.instanceOf(Object),
+  /** Override the styles of the icon element */
+  iconStyle: PropTypes.instanceOf(Object)
+};
 
-  static defaultProps = {
-    icon: null,
-    selected: false,
-    style: {},
-    labelStyle: {},
-    iconStyle: {}
-  };
+Tab.defaultProps = {
+  icon: null,
+  selected: false,
+  style: {},
+  labelStyle: {},
+  iconStyle: {}
+};
 
-  static contextTypes = {
-    color: PropTypes.string
-  }
-
-  render() {
-    const { onClick, icon, label, selected, style, iconStyle, labelStyle } = this.props;
-    const { color } = this.context;
-
-    return (
-      <button style={getStyles.root(color, selected, style)} onClick={onClick}>
-        {icon ? <div style={getStyles.icon(iconStyle)}>{icon}</div> : null}
-        <span style={getStyles.label(labelStyle)}>{label}</span>
-      </button>
-    );
-  }
-}
+Tab.contextTypes = {
+  color: PropTypes.string
+};
 
 export default pure(Radium(Tab));
