685a70e75b335469588aff623b22a08020de6072
Sjaak Luthart <mail@sjaakluthart.nl>
2017-03-22 09:09:00 +0100

    :art: Added getStyles

---
+++ src/input/get-styles.js
@@ -0,0 +1,26 @@
+import styles from './styles';
+import combineStyles from '../internal/combine-styles';
+
+function root(disabled, overrideStyle) {
+  let style = styles.root;
+
+  if (disabled) {
+    style = combineStyles(style, styles.disabled);
+  }
+
+  return combineStyles(style, overrideStyle);
+}
+
+function label(overrideStyle) {
+  return combineStyles(styles.label, overrideStyle);
+}
+
+function input(overrideStyle) {
+  return combineStyles(styles.input, overrideStyle);
+}
+
+export default {
+  root,
+  label,
+  input
+};
