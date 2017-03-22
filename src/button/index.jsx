725f341b4bda372249237203f0be94748938e15d
Sjaak Luthart <mail@sjaakluthart.nl>
2017-03-22 09:13:23 +0100

    :art: Added getStyles

--- src/button/index.jsx
+++ src/button/index.jsx
@@ -1,38 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import shallowEqual from 'recompose/shallowEqual';
 import Radium from 'radium';
-import styles from './styles';
-import colors from '../settings/colors';
-import darken from '../internal/darken';
-import combineStyles from '../internal/combine-styles';
-
-function getStyle(themeColor, inverted, iconButton, disabled, overrideStyle) {
-  const color = themeColor || colors.theme;
-
-  let style = combineStyles(
-    styles.button,
-    {
-      backgroundColor: color,
-      ':hover': { backgroundColor: darken(color, 0.15) },
-      ':active': { backgroundColor: darken(color, 0.25) }
-    }
-  );
-  const invertedStyle = combineStyles(styles.inverted, { color });
-
-  if (iconButton) {
-    style = styles.iconButton;
-  }
-
-  if (disabled) {
-    style = combineStyles(style, styles.disabled);
-  }
-
-  if (inverted) {
-    return combineStyles(combineStyles(style, invertedStyle), overrideStyle);
-  }
-
-  return combineStyles(style, overrideStyle);
-}
+import getStyles from './get-styles';
 
 /** General purpose button with two types */
 class Button extends Component {
@@ -82,7 +51,7 @@ class Button extends Component {
       <button
         key="button"
         onClick={onClick}
-        style={getStyle(color, inverted, iconButton, disabled, style)}
+        style={getStyles.root(color, inverted, iconButton, disabled, style)}
         disabled={disabled}
         {...custom}
       >
