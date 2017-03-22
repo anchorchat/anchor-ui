78ca6da63a68f21792f0c85fcdb7e905f5731b1b
Sjaak Luthart <mail@sjaakluthart.nl>
2017-03-22 08:50:52 +0100

    :sparkles: Added disabled and custom props to Button

--- src/button/index.jsx
+++ src/button/index.jsx
@@ -1,12 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import shallowEqual from 'recompose/shallowEqual';
 import Radium from 'radium';
-import styles from '../style/buttons';
+import styles from './styles';
 import colors from '../settings/colors';
 import darken from '../internal/darken';
 import combineStyles from '../internal/combine-styles';
 
-function getStyle(themeColor, inverted, iconButton, overrideStyle) {
+function getStyle(themeColor, inverted, iconButton, disabled, overrideStyle) {
   const color = themeColor || colors.theme;
 
   let style = combineStyles(
@@ -27,29 +27,38 @@ function getStyle(themeColor, inverted, iconButton, overrideStyle) {
     style = styles.iconButton;
   }
 
+  if (disabled) {
+    style = combineStyles(style, styles.disabled);
+  }
+
   return combineStyles(style, overrideStyle);
 }
 
 /** General purpose button with two types */
 class Button extends Component {
-  static displayName = 'Button';
+  static displayName = 'Button'
+
   static propTypes = {
     /** Content of the button */
     children: PropTypes.node.isRequired,
     /** Button onClick function */
-    onClick: PropTypes.func.isRequired,
+    onClick: PropTypes.func,
     /** Override the styles of the root element */
     style: PropTypes.instanceOf(Object),
     /** Switches between 'icon-button' and 'normal-button' */
     iconButton: PropTypes.bool,
     /** Inverts color */
-    inverted: PropTypes.bool
+    inverted: PropTypes.bool,
+    /** Disables the button */
+    disabled: PropTypes.bool
   }
 
   static defaultProps = {
     style: {},
     iconButton: false,
-    inverted: false
+    inverted: false,
+    onClick: null,
+    disabled: false
   }
 
   static contextTypes = {
@@ -66,11 +75,17 @@ class Button extends Component {
   }
 
   render() {
-    const { children, onClick, iconButton, inverted, style } = this.props;
+    const { children, onClick, iconButton, inverted, style, disabled, ...custom } = this.props;
     const { color } = this.context;
 
     return (
-      <button key="button" onClick={onClick} style={getStyle(color, inverted, iconButton, style)}>
+      <button
+        key="button"
+        onClick={onClick}
+        style={getStyle(color, inverted, iconButton, disabled, style)}
+        disabled={disabled}
+        {...custom}
+      >
         {children}
       </button>
     );
