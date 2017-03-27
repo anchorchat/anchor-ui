a3c59fa55cb63d3f4fab680e7f45eaea2088e491
Sjaak Luthart <mail@sjaakluthart.nl>
2017-03-27 14:05:16 +0200

    :bug: Fixed styling issues with Tabs

--- src/tabs/styles.js
+++ src/tabs/styles.js
@@ -1,16 +1,9 @@
 export default {
-  root: {
-    height: '100%'
-  },
   tabContainer: {
     display: 'flex'
   },
-  contentContainer: {
-    height: 'calc(100% - 50px)'
-  },
   tabContent: {
     width: '100%',
-    height: '100%',
     display: 'none'
   }
 };
