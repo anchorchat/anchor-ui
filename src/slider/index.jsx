7cb90d86cf19bacce9534fdc8057ca3c65b59d05
IanCStewart <ian.c.stewart@icloud.com>
2017-04-01 14:46:23 +0200

    :sparkles: Added custom `PropType` validations.

--- src/slider/index.jsx
+++ src/slider/index.jsx
@@ -37,21 +37,49 @@ function Slider({
   );
 }
 
+const minMaxProptype = (props, propName, componentName, ...rest) => {
+  const error = PropTypes.number(props, propName, componentName, ...rest);
+  if (error !== null) {
+    return error;
+  }
+
+  if (props.min >= props.max) {
+    const errorMsg = (propName === 'min') ? 'min should be less than max' : 'max should be greater than min';
+    return new Error(errorMsg);
+  }
+
+  return null;
+};
+
+const valueInRangePropType = (props, propName, componentName, ...rest) => {
+  const error = PropTypes.number(props, propName, componentName, ...rest);
+  if (error !== null) {
+    return error;
+  }
+
+  const value = props[propName];
+  if (value < props.min || props.max < value) {
+    return new Error(`${propName} should be within the range specified by min and max`);
+  }
+
+  return null;
+};
+
 Slider.displayName = 'Slider';
 
 Slider.propTypes = {
   /** Change the slider's value */
   onChange: PropTypes.func.isRequired,
-  /** The slider's value */
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  /** The slider's value. Cannot be outside of the range of min and max */
+  value: valueInRangePropType,
   /** The slider's label */
   label: PropTypes.node.isRequired,
   /** The slider's name */
   name: PropTypes.string.isRequired,
-  /** The slider's max length */
-  max: PropTypes.number.isRequired,
-  /** The slider's min length */
-  min: PropTypes.number.isRequired,
+  /** The slider's max length. Cannot be equal to min. */
+  max: minMaxProptype,
+  /** The slider's min length. Cannot be equal to max. */
+  min: minMaxProptype,
   /** The slider's step length */
   step: PropTypes.number,
   /** Ref function to the element */
@@ -65,6 +93,9 @@ Slider.propTypes = {
 };
 
 Slider.defaultProps = {
+  max: 1,
+  min: 0,
+  value: 0,
   step: null,
   sliderRef: null,
   disabled: false,
