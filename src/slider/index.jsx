39e198f302798e3896e17c22368dc50fc6c8ca82
IanCStewart <ian.c.stewart@icloud.com>
2017-04-01 15:55:32 +0200

    :lipstick: Testing percentage styles.

--- src/slider/index.jsx
+++ src/slider/index.jsx
@@ -1,41 +1,6 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Radium from 'radium';
 import pure from 'recompose/pure';
-
-/** General purpose form slider */
-function Slider({
-  onChange,
-  value,
-  max,
-  min,
-  step,
-  label,
-  name,
-  sliderRef,
-  disabled,
-  error,
-  errorStyle,
-  ...custom
-}) {
-  return (
-    <section>
-      <label htmlFor={name}>{label}</label>
-      <input
-        onChange={onChange}
-        value={value}
-        type="range"
-        max={max}
-        min={min}
-        id={name}
-        step={step}
-        ref={sliderRef}
-        disabled={disabled}
-        {...custom}
-      />
-      {error ? <span style={errorStyle}>{error}</span> : null}
-    </section>
-  );
-}
 
 const minMaxProptype = (props, propName, componentName, ...rest) => {
   const error = PropTypes.number(props, propName, componentName, ...rest);
@@ -65,42 +30,99 @@ const valueInRangePropType = (props, propName, componentName, ...rest) => {
   return null;
 };
 
-Slider.displayName = 'Slider';
+function getPercent(value, min, max) {
+  let percent = (value - min) / (max - min);
+  if (isNaN(percent)) {
+    percent = 0;
+  }
 
-Slider.propTypes = {
-  /** Change the slider's value */
-  onChange: PropTypes.func.isRequired,
-  /** The slider's value. Cannot be outside of the range of min and max */
-  value: valueInRangePropType,
-  /** The slider's label */
-  label: PropTypes.node.isRequired,
-  /** The slider's name */
-  name: PropTypes.string.isRequired,
-  /** The slider's max length. Cannot be equal to min. */
-  max: minMaxProptype,
-  /** The slider's min length. Cannot be equal to max. */
-  min: minMaxProptype,
-  /** The slider's step length */
-  step: PropTypes.number,
-  /** Ref function to the element */
-  sliderRef: PropTypes.func,
-  /** Disables the slider */
-  disabled: PropTypes.bool,
-  /** Display an error message */
-  error: PropTypes.node,
-  /** Override the styles of the label element */
-  errorStyle: PropTypes.instanceOf(Object)
-};
+  return percent;
+}
 
-Slider.defaultProps = {
-  max: 1,
-  min: 0,
-  value: 0,
-  step: null,
-  sliderRef: null,
-  disabled: false,
-  error: null,
-  errorStyle: {}
-};
+/** General purpose form slider */
+class Slider extends Component {
+  static displayName = 'Slider';
+
+  static propTypes = {
+    /** Change the slider's value */
+    onChange: PropTypes.func.isRequired,
+    /** The slider's value. Cannot be outside of the range of min and max */
+    value: valueInRangePropType,
+    /** The slider's label */
+    label: PropTypes.node.isRequired,
+    /** The slider's name */
+    name: PropTypes.string.isRequired,
+    /** The slider's max length. Cannot be equal to min. */
+    max: minMaxProptype,
+    /** The slider's min length. Cannot be equal to max. */
+    min: minMaxProptype,
+    /** The slider's step length */
+    step: PropTypes.number,
+    /** Ref function to the element */
+    sliderRef: PropTypes.func,
+    /** Disables the slider */
+    disabled: PropTypes.bool,
+    /** Display an error message */
+    error: PropTypes.node,
+    /** Override the styles of the label element */
+    errorStyle: PropTypes.instanceOf(Object)
+  };
+
+  static defaultProps = {
+    max: 1,
+    min: 0,
+    value: 0,
+    step: null,
+    sliderRef: null,
+    disabled: false,
+    error: null,
+    errorStyle: {}
+  };
+
+  render() {
+    const {
+      name,
+      label,
+      onChange,
+      value,
+      max,
+      min,
+      step,
+      sliderRef,
+      disabled,
+      errorStyle,
+      error,
+      ...custom
+    } = this.props;
+    const percent = getPercent(value, min, max);
+
+    return (
+      <section>
+        <label htmlFor={name}>{label}</label>
+        <div style={{ height: '18px', width: '100%', position: 'relative', marginTop: '24px', marginBottom: '48px' }}>
+          <div style={{ position: 'absolute', top: '8px', left: '0px', width: '100%', height: '2px' }}>
+            <div style={{ background: 'blue', width: `${percent * 100}%`, height: '100%', position: 'absolute', left: 0, top: 0 }} />
+            <div style={{ background: 'grey', width: `${(1 - percent) * 100}%`, height: '100%', position: 'absolute', right: 0, top: 0 }} />
+            <div style={{ position: 'absolute', width: '10px', height: '10px', left: `${percent * 100}%`, top: 0, background: 'black', transform: 'translate(-50%, -50%)' }} />
+          </div>
+        </div>
+        <input
+          style={{ margin: 0 }}
+          onChange={onChange}
+          value={value}
+          type="range"
+          max={max}
+          min={min}
+          id={name}
+          step={step}
+          ref={sliderRef}
+          disabled={disabled}
+          {...custom}
+        />
+        {error ? <span style={errorStyle}>{error}</span> : null}
+      </section>
+    );
+  }
+}
 
 export default pure(Radium(Slider));
