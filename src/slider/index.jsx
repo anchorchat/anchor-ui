374f7d74587c4f3caedd5c341de12915eac16d16
Sjaak Luthart <mail@sjaakluthart.nl>
2017-04-02 13:30:44 +0200

    :white_check_mark: Added propTypes

--- src/slider/index.jsx
+++ src/slider/index.jsx
@@ -4,34 +4,7 @@ import pure from 'recompose/pure';
 import styles from './styles';
 import getStyles from './get-styles';
 import getPercentage from '../internal/get-percentage';
-
-const minMaxProptype = (props, propName, componentName, ...rest) => {
-  const error = PropTypes.number(props, propName, componentName, ...rest);
-  if (error !== null) {
-    return error;
-  }
-
-  if (props.min >= props.max) {
-    const errorMsg = (propName === 'min') ? 'min should be less than max' : 'max should be greater than min';
-    return new Error(errorMsg);
-  }
-
-  return null;
-};
-
-const valueInRangePropType = (props, propName, componentName, ...rest) => {
-  const error = PropTypes.number(props, propName, componentName, ...rest);
-  if (error !== null) {
-    return error;
-  }
-
-  const value = props[propName];
-  if (value < props.min || props.max < value) {
-    return new Error(`${propName} should be within the range specified by min and max`);
-  }
-
-  return null;
-};
+import propTypes from '../internal/prop-types';
 
 /** General purpose form slider */
 function Slider({
@@ -85,15 +58,15 @@ Slider.propTypes = {
   /** Change the slider's value */
   onChange: PropTypes.func.isRequired,
   /** The slider's value. Must between min and max */
-  value: valueInRangePropType,
+  value: propTypes.valueInRange,
   /** The slider's label */
   label: PropTypes.node.isRequired,
   /** The slider's name */
   name: PropTypes.string.isRequired,
   /** The slider's max value. Cannot be equal to min. */
-  max: minMaxProptype,
+  max: propTypes.minMax,
   /** The slider's min value. Cannot be equal to max. */
-  min: minMaxProptype,
+  min: propTypes.minMax,
   /** The slider's step value */
   step: PropTypes.number,
   /** Disables the slider */
