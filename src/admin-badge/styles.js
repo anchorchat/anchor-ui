7d22f809777081e29fb088cf2769441f65d0711d
Sjaak Luthart <mail@sjaakluthart.nl>
2017-04-13 10:06:39 +0200

    :lipstick: Changed AdminBadge style

--- src/admin-badge/styles.js
+++ src/admin-badge/styles.js
@@ -1,17 +1,16 @@
 import colors from '../settings/colors';
-import styles from '../settings/styles';
 
 const styleSheet = {
   root: {
     backgroundColor: colors.theme,
-    boxShadow: styles.depthShadows[0],
     color: colors.white,
     fontSize: '10px',
     lineHeight: '10px',
     padding: '3px',
     textAlign: 'center',
     borderRadius: '3px',
-    fontFamily: 'inherit'
+    fontFamily: 'inherit',
+    border: `1px solid ${colors.theme}`
   },
   inverted: {
     color: colors.theme,
