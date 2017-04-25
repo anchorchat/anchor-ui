9eacbe8838cc8e9e73ea64eec48f5345a630d16e
tomoversluizen <tom.oversluizen@hotmail.com>
2017-04-25 16:50:28 +0200

    test for the input

---
+++ test/input/get-styles.test.js
@@ -0,0 +1,55 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import getStyles from '../../src/input/get-styles';
+import styles from '../../src/input/styles';
+
+describe('Input.getStyles', () => {
+  describe('root', () => {
+    it('should get styles', () => {
+      const style = getStyles.root();
+
+      expect(style).to.deep.equal(styles.root);
+    });
+    it('should change letter color', () => {
+      const style = getStyles.root(false, { color: 'green' });
+
+      expect(style).to.have.property('color', 'green');
+    });
+
+    it('should add disabled styles', () => {
+      const style = getStyles.root(true, {});
+
+      expect(style).to.have.property('opacity', '0.38');
+    });
+  });
+
+  describe('label', () => {
+    it('should get styles', () => {
+      const style = getStyles.label();
+
+      expect(style).to.deep.equal(styles.label);
+    });
+
+    it('should combine styles', () => {
+      const style = getStyles.label(false, { display: 'block' });
+
+      expect(style).to.have.property('display', 'block');
+    });
+  });
+
+  describe('error', () => {
+    it('should disable styles', () => {
+      const style = getStyles.error(false, {});
+
+      expect(style).to.have.property('fontSize', '14px');
+    });
+  });
+
+  describe('input', () => {
+    it('should get styles', () => {
+      const style = getStyles.input(true, {});
+
+      expect(style).to.have.property('width', '100%');
+    });
+  });
+});
