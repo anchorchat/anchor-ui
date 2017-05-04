621a9ed5d4ff8da2760fd03628c2355f4d22c8cb
Ralphvandodewaard <rtvd@ziggo.nl>
2017-05-04 10:56:28 +0200

    Fixed last few "style={{ }}"s

--- src/list-item/index.jsx
+++ src/list-item/index.jsx
@@ -146,7 +146,7 @@ class ListItem extends Component {
     }
 
     return (
-      <div style={{ width: '100%' }}>
+      <div>
         <li key="listItem" onClick={onClick} style={getStyles.root(color, active, rightButton, avatar, style)} {...custom}>
           {
             avatar
