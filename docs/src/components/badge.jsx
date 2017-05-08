c8071dbf2454c5b115be6b4c9930cf4fddf6e5bb
Sjaak Luthart <mail@sjaakluthart.nl>
2017-05-08 08:38:09 +0200

    :art: Changed to single lines

--- docs/src/components/badge.jsx
+++ docs/src/components/badge.jsx
@@ -35,22 +35,9 @@ function BadgeDoc() {
       <section>
         <h1>Examples</h1>
         <Paper style={style.paper}>
-          <Badge
-            style={style.badge}
-            value={9}
-            maxValue={9}
-          />
-          <Badge
-            style={style.badge}
-            value={10}
-            maxValue={9}
-          />
-          <Badge
-            style={style.badge}
-            value={10}
-            maxValue={9}
-            inverted
-          />
+          <Badge style={style.badge} value={9} maxValue={9} />
+          <Badge style={style.badge} value={10} maxValue={9} />
+          <Badge style={style.badge} value={10} maxValue={9} inverted />
         </Paper>
       </section>
       <Props props={componentData.props} />
