abb5525f7333ada807fa2bec6246abfaa48af241
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-05-10 10:35:04 +0200

    [3.0.6] Added pure to WithTheme, added status to Avatar. (#486)
    * Added more tables. (#452)
    
    * added text overflow to list-item (#455)
    
    * added text overflow to list-item
    
    * deleted comma at line 65
    
    * :bug: Added working fix
    
    * Saved styles to object for all pages (#457)
    
    * Added more examples to docs (#450)
    
    * Added more input examples
    
    * Added more message examples
    
    * Added more listItem examples
    
    * Saved style objects into consts
    
    * Saved styles into one object
    
    * Added error example on Select page and saved styles into object
    
    * Added more Slider examples
    
    * Added error style to Slider (Sjaak)
    
    * Saved styles into object for Input and Message
    
    * Changed valueDefaults to values and placed closing brackets on newline
    
    * Issue #456 (#458)
    
    * :bug: Added text overlfow to Profile
    
    * :bug: Added text overflow to channel header
    
    * :bug: Added text overflow to pop over
    
    * :bug: Added textOverflow to AppHeader
    
    * :bug: Added text overflow to menu item
    
    * Add input example (#459)
    
    * Added more input examples
    
    * Added more message examples
    
    * Added more listItem examples
    
    * Saved style objects into consts
    
    * Saved styles into one object
    
    * Added error example on Select page and saved styles into object
    
    * Added more Slider examples
    
    * Added error style to Slider (Sjaak)
    
    * Saved styles into object for Input and Message
    
    * Changed valueDefaults to values and placed closing brackets on newline
    
    * :bug: Added disabled styles
    
    * :lipstick: Changed color
    
    * :fire: Removed inputs
    
    * Added error and disabled styles to Slider, fixed text overflow issues.
    
    * Issue-#462 (#464)
    
    * listItem set secondaryText lineHeight to 16px
    
    * Increased listItem height and fixed lineHeight
    
    * Fixed lineheight in list and updated examples
    
    * modified the max-height of the messageImage (#466)
    
    * modified the max-height of the messageImage
    
    * deleted commets and narrowed the max height down to 200px
    
    * Added more verticl spacing between Title, Description, Usage, Examples and Props (#468)
    
    * Fixed styling for Input page (#471)
    
    * Fixed styling for Input page
    
    * :lipstick: Changed Input's background color
    
    * Issue-#451: Fixed last few "style={{ }}"s (#469)
    
    * Fixed last few "style={{ }}"s
    
    * :bug: Reverted changes
    
    * :art: Renamed object
    
    * Issue #446: Added examples to components (#470)
    
    * Added AdminBadge example
    
    * Added Badge example
    
    * Added Button example
    
    * :art: Changed to single lines
    
    * Changed image Message max height, fixed Input background colour, fixed Input label colour.
    
    * Issue #477 (#478)
    
    * :sparkles: Added IconNotification
    
    * :memo: Added docs output
    
    * Issue #447 (#472)
    
    * Fixed styling for Input page
    
    * :sparkles: Added `lightbox/index.jsx`.
    
    * :lipstick: Added styles for `lightbox`.
    
    * :sparkles: Added lightbox to `message.jsx`.
    
     #447
    
    * :memo: Docgen.
    
    * :hankey: Reverted unwanted commit.
    
    * :sparkles: Added `enableLightbox` prop.
    
    * :memo: Docgen.
    
    * :memo: Added `Docs` page.
    
    * :pencil2: New image link.
    
    * :bug: Added more checks for `enableLightbox`.
    
    * :bug: Added more checks.
    
    * :sparkles: Added onMenuClose to IconMenu (#479)
    
    * :sparkles: Added onMenuClose to IconMenu
    
    * :memo: Added docs
    
    * Added Lightbox component, added lightbox to Message, added IconNotification, added onMenuClose to IconMenu.
    
    * :sparkles: Added status indicator to Avatar (#484)
    
    Fixed issue #483
    
    * :zap: Added recompose pure to WithTheme (#485)
    
    Fixed issue #481
    
    * Added pure to WithTheme, added status to Avatar.

--- src/avatar/get-styles.js
+++ src/avatar/get-styles.js
@@ -1,5 +1,6 @@
 import styles from './styles';
 import combineStyles from '../internal/combine-styles';
+import colors from '../settings/colors';
 
 function root(image, overrideStyle) {
   const style = combineStyles(styles.root, { backgroundImage: `url(${image})` });
@@ -7,6 +8,27 @@ function root(image, overrideStyle) {
   return combineStyles(style, overrideStyle);
 }
 
+function status(type, overrideStyle) {
+  let style = styles.status;
+  let backgroundColor;
+
+  switch (type) {
+    case 'away':
+      backgroundColor = colors.away;
+      break;
+    case 'offline':
+      backgroundColor = colors.offline;
+      break;
+    default:
+      backgroundColor = colors.online;
+  }
+
+  style = combineStyles(style, { backgroundColor });
+
+  return combineStyles(style, overrideStyle);
+}
+
 export default {
-  root
+  root,
+  status
 };
