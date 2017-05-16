1a0c0f978cf21bb49056d9cdfdbb807e8be8ab39
Sjaak Luthart <mail@sjaakluthart.nl>
2017-05-16 18:12:14 +0200

    :hammer: Changed back to stateless, added color propType, added compose

--- src/button/index.jsx
+++ src/button/index.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import pure from 'recompose/pure';
+import compose from 'recompose/compose';
 import Radium from 'radium';
 import getStyles from './get-styles';
-import ThemeableComponent from '../internal/themeable-component';
+import themeable from '../internal/themeable-component';
 
 /** General purpose button with three types */
-class Button extends ThemeableComponent {
-  render() {
-    const {
-      children, onClick, iconButton, inverted, style, disabled, flatButton, ...custom
-    } = this.props;
-    const { color } = this.state;
-
-    return (
-      <button
-        onClick={onClick}
-        style={getStyles.root(color, inverted, iconButton, disabled, flatButton, style)}
-        disabled={disabled}
-        {...custom}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+const Button = ({
+  children, onClick, iconButton, inverted, style, disabled, flatButton, color, ...custom
+}) => (
+  <button
+    onClick={onClick}
+    style={getStyles.root(color, inverted, iconButton, disabled, flatButton, style)}
+    disabled={disabled}
+    {...custom}
+  >
+    {children}
+  </button>
+);
 
 Button.displayName = 'Button';
 
@@ -42,7 +36,8 @@ Button.propTypes = {
   /** Disables the button */
   disabled: PropTypes.bool,
   /** Toggle FlatButton type */
-  flatButton: PropTypes.bool
+  flatButton: PropTypes.bool,
+  color: PropTypes.string.isRequired
 };
 
 Button.defaultProps = {
@@ -54,4 +49,10 @@ Button.defaultProps = {
   flatButton: false
 };
 
-export default pure(Radium(Button));
+const enhance = compose(
+  themeable,
+  Radium,
+  pure
+);
+
+export default enhance(Button);
