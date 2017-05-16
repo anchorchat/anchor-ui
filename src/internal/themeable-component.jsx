27a0739e0b8d3071e4f8699cf102b1cb8f6d2686
Sjaak Luthart <mail@sjaakluthart.nl>
2017-05-16 18:11:09 +0200

    :construction: Added themeable HoC

---
+++ src/internal/themeable-component.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+
+const themeable = (ChildComponent) => {
+  class ThemeableComponent extends Component {
+    static contextTypes = {
+      theme: PropTypes.object
+    }
+
+    constructor(props, context) {
+      super(props, context);
+
+      this.state = {
+        color: this.context.theme.color
+      };
+    }
+
+    componentDidMount() {
+      this.unsubscribe = this.context.theme.subscribe(this.setColor);
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
+
+    setColor = (color) => {
+      console.log('new color', color);
+      this.setState({ color });
+    };
+
+    render() {
+      return <ChildComponent {...this.props} color={this.state.color} />;
+    }
+  }
+
+  return ThemeableComponent;
+};
+
+export default themeable;
