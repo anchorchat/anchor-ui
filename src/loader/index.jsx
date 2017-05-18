1d24a6a95fb4be27bb0e1ec7cd198a855730e581
Sjaak Luthart <mail@sjaakluthart.nl>
2017-05-18 16:44:55 +0200

    :fire: Removed styleroot

--- src/loader/index.jsx
+++ src/loader/index.jsx
@@ -1,36 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleRoot } from 'radium';
-import Loader from './loader';
+import Radium from 'radium';
+import compose from 'recompose/compose';
+import styles from '../style/loaders';
+import colors from '../settings/colors';
+import combineStyles from '../internal/combine-styles';
+import themeable from '../themeable';
 
-// StyleRoot is needed for keyframes to work,
-// by exporting it like this a consumer doesn't need to wrap StyleRoot around their application
-// https://github.com/FormidableLabs/radium/tree/master/docs/api#keyframes
-// https://github.com/FormidableLabs/radium/tree/master/docs/api#styleroot-component
-function LoaderWithStyleRoot(props) {
-  const {
-    dotStyle, // eslint-disable-line no-unused-vars
-    inverted, // eslint-disable-line no-unused-vars
-    ...custom
-  } = props;
+function getStyle(color = colors.theme, inverted, index, overrideStyle) {
+  let style = combineStyles(styles.dot, { backgroundColor: color });
 
-  return (
-    <StyleRoot {...custom}>
-      <Loader {...props} />
-    </StyleRoot>
-  );
+  if (index === 1) {
+    style = combineStyles(style, { animationDelay: '.33s' });
+  }
+
+  if (index === 2) {
+    style = combineStyles(style, { animationDelay: '.66s' });
+  }
+
+  if (inverted) {
+    return combineStyles(combineStyles(style, styles.inverted), overrideStyle);
+  }
+
+  return combineStyles(style, overrideStyle);
 }
 
-LoaderWithStyleRoot.propTypes = {
+/** Animated loader */
+const Loader = ({ inverted, style, dotStyle, color, ...custom }) => (
+  <div style={combineStyles(styles.loader, style)} {...custom}>
+    <span style={getStyle(color, inverted, 0, dotStyle)} />
+    <span style={getStyle(color, inverted, 1, dotStyle)} />
+    <span style={getStyle(color, inverted, 2, dotStyle)} />
+  </div>
+);
+
+Loader.displayName = 'Loader';
+
+Loader.propTypes = {
+  /** Override the styles of the root element */
+  style: PropTypes.instanceOf(Object),
   /** Override the styles of the dot element */
   dotStyle: PropTypes.instanceOf(Object),
   /** Inverts the color */
-  inverted: PropTypes.bool
+  inverted: PropTypes.bool,
+  color: PropTypes.string.isRequired
 };
 
-LoaderWithStyleRoot.defaultProps = {
+Loader.defaultProps = {
+  style: {},
   dotStyle: {},
   inverted: false
 };
 
-export default LoaderWithStyleRoot;
+const enhance = compose(
+  themeable(),
+  Radium
+);
+
+export default enhance(Loader);
