e56ef68bf990cbb2ebf260972ad440e854d33945
Ralphvandodewaard <ralph.van.dodewaard@anchor.chat>
2017-05-19 18:32:45 +0200

    Issue #203 loader (#558)
    * Refactored loader style function
    
    * Added test for loader style function, #203
    
    * Fixed naming
    
    * Added root style function
    
    * Fixed function, added test

---
+++ src/loader/get-styles.js
@@ -0,0 +1,30 @@
+import styles from './styles';
+import combineStyles from '../internal/combine-styles';
+import colors from '../settings/colors';
+
+function root(overrideStyle) {
+  return combineStyles(styles.root, overrideStyle);
+}
+
+function dot(color = colors.theme, inverted, index, overrideStyle) {
+  let style = combineStyles(styles.dot, { backgroundColor: color });
+
+  if (index === 1) {
+    style = combineStyles(style, { animationDelay: '.33s' });
+  }
+
+  if (index === 2) {
+    style = combineStyles(style, { animationDelay: '.66s' });
+  }
+
+  if (inverted) {
+    return combineStyles(combineStyles(style, styles.inverted), overrideStyle);
+  }
+
+  return combineStyles(style, overrideStyle);
+}
+
+export default {
+  root,
+  dot
+};
---
+++ test/loader/get-styles.test.js
@@ -0,0 +1,58 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import getStyles from '../../src/loader/get-styles';
+import styles from '../../src/loader/styles';
+
+describe('Loader.getStyles', () => {
+  describe('root', () => {
+    it('should get styles', () => {
+      const style = getStyles.root();
+
+      expect(style).to.deep.equal(styles.root);
+    });
+
+    it('should combine styles', () => {
+      const style = getStyles.root({ color: 'red' });
+
+      expect(style).to.have.property('color', 'red');
+    });
+  });
+
+  describe('dot', () => {
+    it('should get styles', () => {
+      const style = getStyles.dot();
+
+      expect(style).to.deep.equal(styles.dot);
+    });
+
+    it('should combine styles', () => {
+      const style = getStyles.dot('red', false, 1, { color: 'red' });
+
+      expect(style).to.have.property('color', 'red');
+    });
+
+    it('should change theme color', () => {
+      const style = getStyles.dot('red');
+
+      expect(style).to.have.property('backgroundColor', 'red');
+    });
+
+    it('should add inverted styles', () => {
+      const style = getStyles.dot('red', true);
+
+      expect(style).to.have.property('backgroundColor', '#FEFEFE');
+    });
+
+    it('should add index 1 styles', () => {
+      const style = getStyles.dot('red', false, 1);
+
+      expect(style).to.have.property('animationDelay', '.33s');
+    });
+
+    it('should add index 2 styles', () => {
+      const style = getStyles.dot('red', false, 2);
+
+      expect(style).to.have.property('animationDelay', '.66s');
+    });
+  });
+});
