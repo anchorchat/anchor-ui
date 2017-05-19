474abe2e1e2804ec3d69fafe4d619414b3286ecc
Ralphvandodewaard <rtvd@ziggo.nl>
2017-05-19 08:49:49 +0200

    Fixed empty objects and changed to es6 default argument

--- src/badge/get-styles.js
+++ src/badge/get-styles.js
@@ -2,9 +2,7 @@ import colors from '../settings/colors';
 import combineStyles from '../internal/combine-styles';
 import styles from './styles';
 
-function root(themeColor, inverted, overrideStyle) {
-  const color = themeColor || colors.theme;
-
+function root(color = colors.theme, inverted, overrideStyle) {
   const style = combineStyles(styles.badge, { backgroundColor: color });
   const invertedStyle = combineStyles(styles.inverted, { color });
 
