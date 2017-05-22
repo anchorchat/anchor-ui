3179ebf4f32840cd44d1b1b9df7dc9cce09172a3
Ralphvandodewaard <rtvd@ziggo.nl>
2017-05-22 14:21:36 +0200

    Added test for profile-card style function, #203

---
+++ test/profile-card/get-styles.test.js
@@ -0,0 +1,32 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import getStyles from '../../src/profile-card/get-styles';
+import styles from '../../src/profile-card/styles';
+
+describe('ProfileCard.getStyles', () => {
+  describe('root', () => {
+    it('should get styles', () => {
+      const style = getStyles.root();
+
+      expect(style).to.deep.equal(styles.profileCard);
+    });
+
+    it('should combine styles', () => {
+      const style = getStyles.root('red', false, { color: 'red' });
+
+      expect(style).to.have.property('color', 'red');
+    });
+
+    it('should add theme colors', () => {
+      const style = getStyles.root('red');
+
+      expect(style).to.have.property('backgroundColor', 'red');
+    });
+
+    it('should add avatar styles', () => {
+      const style = getStyles.root('red', true);
+
+      expect(style).to.have.property('lineHeight', '40px');
+    });
+  });
+});
