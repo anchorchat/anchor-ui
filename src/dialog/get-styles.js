d729a0c6c3e93abe0a88185836ae11e7264a8a4e
tomoversluizen <tom.oversluizen@hotmail.com>
2017-05-22 11:51:15 +0200

    updated dialog getstyles function

--- src/dialog/get-styles.js
+++ src/dialog/get-styles.js
@@ -2,14 +2,13 @@ import combineStyles from '../internal/combine-styles';
 import colors from '../settings/colors';
 import styles from './styles';
 
-function root(color = colors.theme, overrideStyle) {
+const root = (color = colors.theme, overrideStyle) => {
   const style = combineStyles(styles.dialog, { backgroundColor: color });
 
   return combineStyles(style, overrideStyle);
-}
+};
 
-function header(overrideStyle) {
-  return combineStyles(styles.header, overrideStyle);
-}
+const header = overrideStyle => combineStyles(styles.header, overrideStyle);
+
 
 export default { root, header };
