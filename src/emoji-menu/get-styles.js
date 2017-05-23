e62a3ee7399f8f1c6884a2c4e4d2402bee115fd2
Ralphvandodewaard <rtvd@ziggo.nl>
2017-05-23 12:07:56 +0200

    Refactored emoji-categories styles

--- src/emoji-menu/get-styles.js
+++ src/emoji-menu/get-styles.js
@@ -5,6 +5,16 @@ function root(overrideStyle) {
   return combineStyles(styles.root, overrideStyle);
 }
 
+function categories(overrideStyle) {
+  return combineStyles(styles.categories, overrideStyle);
+}
+
+function category(overrideStyle) {
+  return combineStyles(styles.categories.category, overrideStyle);
+}
+
 export default {
-  root
+  root,
+  categories,
+  category
 };
