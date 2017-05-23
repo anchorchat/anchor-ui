19d42be39457766c8c4942bad1e051c945c106b8
Ralphvandodewaard <rtvd@ziggo.nl>
2017-05-23 11:03:58 +0200

    Small fix

--- src/divider/get-styles.js
+++ src/divider/get-styles.js
@@ -1,11 +1,11 @@
 import styles from './styles';
 import combineStyles from '../internal/combine-styles';
 
-function hr(type, overrideStyle) {
+function hr(overrideStyle) {
   return combineStyles(styles.hr, overrideStyle);
 }
 
-function text(type, overrideStyle) {
+function text(overrideStyle) {
   return combineStyles(styles.text, overrideStyle);
 }
 
