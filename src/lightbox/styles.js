3bc54105c33c928996c908fb75f3ac1d516e9556
IanCStewart <ian.c.stewart@icloud.com>
2017-05-26 11:46:21 +0200

    :rewind: Reverted develop push.
    @iancstewart didn't have his coffee yet.

--- src/lightbox/styles.js
+++ src/lightbox/styles.js
@@ -1,17 +1,17 @@
+import colors from '../settings/colors';
+import styles from '../settings/styles';
+
 export default {
   lightbox: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
+    height: '80%',
+    maxWidth: '100%',
     margin: '0 auto',
+    background: colors.white,
     borderRadius: '3px',
     padding: '40px 30px',
     boxSizing: 'border-box',
     position: 'relative',
-    pointerEvents: 'none'
+    boxShadow: styles.depthShadows[0]
   },
   closeButton: {
     position: 'absolute',
@@ -20,9 +20,10 @@ export default {
     zIndex: 1
   },
   image: {
+    width: 'auto',
+    height: 'auto',
     maxWidth: '100%',
-    maxHeight: '100%',
-    pointerEvents: 'all'
+    maxHeight: '100%'
   },
   clickAway: {
     width: '100%',
