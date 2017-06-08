9be102bf16a225f6b8da9de00ea376daacf767e8
Ralphvandodewaard <rtvd@ziggo.nl>
2017-06-08 08:48:51 +0200

    Changed naming of profile to wrapper

--- test/profile/index.test.js
+++ test/profile/index.test.js
@@ -32,62 +32,62 @@ describe('Profile.index', () => {
   };
 
   it('should always render four section elements', () => {
-    const profile = shallow(<Profile {...props} />);
+    const wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find('section')).to.have.length(4);
+    expect(wrapper.find('section')).to.have.length(4);
   });
 
   it('should always render a h1 element', () => {
-    const profile = shallow(<Profile {...props} />);
+    const wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find('h1')).to.have.length(1);
+    expect(wrapper.find('h1')).to.have.length(1);
   });
 
   it('should pass the value of the header prop to the h1 element', () => {
-    const profile = shallow(<Profile {...props} />);
+    const wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.containsMatchingElement(<h1>header</h1>)).to.equal(true);
+    expect(wrapper.containsMatchingElement(<h1>header</h1>)).to.equal(true);
   });
 
   it('should always render the value of the button prop', () => {
     props.button = <button>text</button>;
-    const profile = shallow(<Profile {...props} />);
+    const wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find('button')).to.have.length(1);
-    expect(profile.containsMatchingElement(<button>text</button>)).to.equal(true);
+    expect(wrapper.find('button')).to.have.length(1);
+    expect(wrapper.containsMatchingElement(<button>text</button>)).to.equal(true);
     props.button = null;
   });
 
   it('should always render the value of the children prop', () => {
-    const profile = shallow(<Profile {...props} >{children}</Profile>);
+    const wrapper = shallow(<Profile {...props} >{children}</Profile>);
 
-    expect(profile.contains(children)).to.equal(true);
+    expect(wrapper.contains(children)).to.equal(true);
   });
 
   it('should render a p element if the secondaryText prop is passed', () => {
-    let profile = shallow(<Profile {...props} />);
+    let wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find('p')).to.have.length(0);
+    expect(wrapper.find('p')).to.have.length(0);
 
     props.secondaryText = 'text';
-    profile = shallow(<Profile {...props} />);
+    wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find('p')).to.have.length(1);
-    expect(profile.containsMatchingElement(<p>text</p>)).to.equal(true);
+    expect(wrapper.find('p')).to.have.length(1);
+    expect(wrapper.containsMatchingElement(<p>text</p>)).to.equal(true);
     props.secondaryText = '';
   });
 
   it('should not render an Avatar component if the avatar prop is not passed', () => {
-    const profile = shallow(<Profile {...props} />);
+    const wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find(Avatar)).to.have.length(0);
+    expect(wrapper.find(Avatar)).to.have.length(0);
   });
 
   it('should render an Avatar component if the avatar prop is passed', () => {
     props.avatar = 'imageurl';
-    const profile = shallow(<Profile {...props} />);
+    const wrapper = shallow(<Profile {...props} />);
 
-    expect(profile.find(Avatar)).to.have.length(1);
+    expect(wrapper.find(Avatar)).to.have.length(1);
     props.avatar = '';
   });
 
