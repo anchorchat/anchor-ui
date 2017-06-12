9856ffa5d372c1b36131aa0312166e8852c8553f
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-06-12 12:14:45 +0200

    :art: Render all icons in loop

--- docs/src/components/icons.jsx
+++ docs/src/components/icons.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import _ from 'underscore';
-import * as Icon from '../../../dist/icons';
+import uuid from 'uuid';
+import * as icons from '../../../dist/icons';
 import components from '../../components.json';
 import Paper from '../../../dist/paper';
 
 const usage = '```js\n import { IconEmoji } from \'anchor-ui/icons\'; \n import IconHammer from \'anchor-ui/icons/icon-hammer\'';
 
-const Icons = () => {
-  const icons = _.filter(components, (component, key) => key.match(/icons/));
-  const iconNames = _.pluck(icons, 'displayName');
+const IconsDoc = () => {
+  const iconData = _.filter(components, (component, key) => key.match(/icons/));
+  const iconNames = _.pluck(iconData, 'displayName');
   const style = {
     paper: {
       display: 'flex',
@@ -35,17 +36,7 @@ const Icons = () => {
       <section>
         <h1>Examples</h1>
         <Paper style={style.paper}>
-          <Icon.IconActivity style={style.icon} />
-          <Icon.IconAddFriend style={style.icon} />
-          <Icon.IconAddImage style={style.icon} />
-          <Icon.IconAdminPanel style={style.icon} />
-          <Icon.IconAdmin style={style.icon} />
-          <Icon.IconAnalytics style={style.icon} />
-          <Icon.IconBlock style={style.icon} />
-          <Icon.IconChannel style={style.icon} />
-          <Icon.IconChannels style={style.icon} />
-          <Icon.IconCheckbox style={style.icon} />
-          <Icon.IconChevron style={style.icon} />
+          {_.map(icons, icon => React.createElement(icon, { style: style.icon, key: uuid.v4() }))}
         </Paper>
         <h2>Available icons</h2>
         <ul>
@@ -79,4 +70,4 @@ const Icons = () => {
   );
 };
 
-export default Icons;
+export default IconsDoc;
