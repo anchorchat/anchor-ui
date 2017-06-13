aa81d581cb9e9c1833332d58511585766e6165cd
Tom Oversluizen <tom.oversluizen@hotmail.com>
2017-06-13 08:27:19 +0200

    CardContent component test (#679)
    * created card-content component test
    
    * updated component test card-content
    
    * updated component test card-content
    
    * updated component test card-content with an extra test

---
+++ test/card-content/index.test.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+/* eslint react/jsx-filename-extension: [0] */
+import React from 'react';
+import chai, { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import CardContent from '../../src/card-content';
+import getStyles from '../../src/card-content/get-styles';
+
+chai.use(sinonChai);
+
+describe('card-content', () => {
+  const props = {
+    style: {}
+  };
+  const children = <p>children</p>;
+
+  beforeEach(() => {
+    global.navigator = { userAgent: 'all' };
+  });
+
+  afterEach(() => {
+    global.navigator = undefined;
+  });
+
+  it('should always render a section element', () => {
+    const wrapper = shallow(<CardContent {...props}>{children}</CardContent>).dive();
+
+    expect(wrapper.find('section')).to.have.length(1);
+  });
+
+  it('should pass the value of the children prop to the section element', () => {
+    const wrapper = shallow(<CardContent {...props}>{children}</CardContent>).dive();
+
+    expect(wrapper.containsMatchingElement(<p>children</p>)).to.equal(true);
+  });
+
+  it('should get root styles', () => {
+    const spy = sinon.spy(getStyles, 'root');
+
+    shallow(<CardContent {...props} >{children}</CardContent>).dive();
+    expect(spy).to.have.been.calledWith(props.style);
+  });
+});
