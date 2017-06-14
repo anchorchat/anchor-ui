08f8c9c1d62d52b8978ff1af9401b7bd3d46cd70
iancstewart <ian.c.stewart@icloud.com>
2017-06-14 10:22:01 +0200

    :lipstick: Added styles for edited messages.

--- src/message/message-time/index.jsx
+++ src/message/message-time/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import format from 'date-fns/format';
 import getStyles from './get-styles';
+import styles from './styles';
 
 const MessageTime = ({ myMessage, type, style, createdAt, timeFormat, edited }) => (
-  <span style={getStyles.root(myMessage, type, style)}>
-    {edited}
-    {format(createdAt, timeFormat)}
+  <span style={getStyles.root(myMessage, type, edited, style)}>
+    {edited ? <span style={styles.edited}>{edited}</span> : null}
+    <span>{format(createdAt, timeFormat)}</span>
   </span>
 );
 
