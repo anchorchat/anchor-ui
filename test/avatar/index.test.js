0d86f82f92dbd60f70e2526f805e6186d3402782
Ralphvandodewaard <rtvd@ziggo.nl>
2017-06-16 12:31:59 +0200

    Fixed describe title for avatar test

--- test/avatar/index.test.js
+++ test/avatar/index.test.js
@@ -10,7 +10,7 @@ import getStyles from '../../src/avatar/get-styles';
 
 chai.use(sinonChai);
 
-describe('Avatar.index', () => {
+describe('Avatar', () => {
   const props = {
     image: 'image',
     style: { root: true },
