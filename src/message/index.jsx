2b5e91992bccbff8650e7553987670b008d4a668
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-06-19 16:28:36 +0200

    Issue #700 (#714)
    * :sparkles: Added DateSeparator
    
    * :sparkles: Added separator prop
    
    * :fire: Removed position relative
    
    * :lipstick: Added flexShrink: 0
    
    * :fire: Removed redundant style
    
    * :globe_with_meridians: Added i18n to DateSeparator
    
    * :memo: Docgen
    
    * :memo: Changed comment
    
    * :heavy_plus_sign: Added escape-html
    
    * :sparkles: Added link parse
    
    * :memo: Doc gen

--- src/message/index.jsx
+++ src/message/index.jsx
@@ -11,6 +11,7 @@ import ImageMessage from './image-message';
 import StickerMessage from './sticker-message';
 import MenuItem from '../menu-item';
 import themeable from '../themeable';
+import styles from './styles';
 
 /** Messages with optional styling for the current user's message,
 different font sizes and message styles */
@@ -34,7 +35,11 @@ class Message extends Component {
       /** The message's type */
       type: PropTypes.oneOf(['text', 'image', 'sticker'])
     }).isRequired,
-    /** The format of displaying message.createdAt */
+    /**
+     * The format of displaying message.createdAt
+     *
+     * https://date-fns.org/docs/format
+     */
     timeFormat: PropTypes.string,
     /** Override the styles of the root element */
     style: PropTypes.instanceOf(Object),
@@ -70,6 +75,8 @@ class Message extends Component {
     expandIcon: PropTypes.node,
     /** Text to display for edited banner */
     edited: PropTypes.node,
+    /** Show a separator above the message */
+    separator: PropTypes.node,
     color: PropTypes.string.isRequired
   }
 
@@ -92,7 +99,8 @@ class Message extends Component {
     expandText: 'Expand image',
     expandIcon: null,
     collapsedText: 'This image has been collapsed, click the button to expand it.',
-    edited: null
+    edited: null,
+    separator: null
   }
 
   constructor() {
@@ -176,6 +184,7 @@ class Message extends Component {
       collapsedText, // eslint-disable-line no-unused-vars
       edited, // eslint-disable-line no-unused-vars
       color,
+      separator,
       ...custom
     } = this.props;
 
@@ -190,10 +199,13 @@ class Message extends Component {
     }
 
     return (
-      <section style={getStyles.container(myMessage, compact)} {...custom}>
-        {messageElement}
-        {message.type === 'image' ? this.renderImageIconMenu() : this.renderIconMenu()}
-      </section>
+      <div style={styles.root}>
+        {separator}
+        <section style={getStyles.container(myMessage, compact)} {...custom}>
+          {messageElement}
+          {message.type === 'image' ? this.renderImageIconMenu() : this.renderIconMenu()}
+        </section>
+      </div>
     );
   }
 }
