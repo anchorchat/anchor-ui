97cb96cb4a98929264ca65a24ce5e53909082f67
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-06-21 19:38:23 +0200

    Fixed parse script.

--- parse-emoji.js
+++ parse-emoji.js
@@ -1,16 +1,18 @@
 const emojione = require('./emojione.json');
 const map = require('lodash/map');
 
-console.log('export default [');
+console.log('export default ['); // eslint-disable-line no-console
 
 map(emojione, (e) => {
-  console.log(`
-    {title: '${e.shortname}',
-    name: '${e.name}',
-    shortname: '${e.shortname}',
-    category: '${e.category}',
-    diversity: ${e.diversities.length > 0 ? true : false}},
-  `);
+  console.log(
+    `{
+      title: '${e.shortname}',
+      name: '${e.name}',
+      shortname: '${e.shortname}',
+      category: '${e.category}',
+      diversity: ${Boolean(e.diversities.length > 0 || e.diversity)}
+    },`
+  );
 });
 
-console.log('];');
+console.log('];'); // eslint-disable-line no-console
