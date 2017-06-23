e45a8193052ef08fdacdb4509261bcbee23810d7
Ian Stewart <IanCStewart@users.noreply.github.com>
2017-06-23 15:41:11 +0200

    :lipstick: Adjusted styles to conform with design. (#701)
    * :lipstick: Adjusted styles to conform with design.
    
    * Added disabled prop to Input, fixed placeholder styles.
    
    * Changed Input text color.
    
    * Changed disabled styles.
    
    * Fixed tests.

--- src/input/styles.js
+++ src/input/styles.js
@@ -7,11 +7,11 @@ export default {
   },
   input: {
     appearance: 'none',
-    backgroundColor: 'rgba(45, 55, 104, 0.75)',
+    backgroundColor: colors.inputs,
     border: '0',
     borderRadius: '3px',
     boxSizing: 'border-box',
-    color: colors.white,
+    color: colors.primaryText,
     fontSize: '16px',
     fontWeight: 'inherit',
     fontFamily: 'inherit',
@@ -35,7 +35,7 @@ export default {
     width: '100%'
   },
   disabled: {
-    opacity: '0.38'
+    opacity: '0.5'
   },
   error: {
     display: 'block',
