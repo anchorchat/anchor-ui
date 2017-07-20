8e37a57889e3b1b388da17f3fdcedcfc105aa2d6
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-07-20 14:26:33 +0200

    Fixed tests

--- test/paper/get-styles.test.js
+++ test/paper/get-styles.test.js
@@ -9,7 +9,7 @@ describe('Paper.getStyles', () => {
       const style = getStyles.root();
 
       expect(style).to.be.an('object');
-      expect(style).to.have.all.keys('padding', 'borderRadius', 'boxShadow', 'background', 'margin');
+      expect(style).to.have.all.keys('padding', 'borderRadius', 'boxShadow', 'backgroundColor', 'margin');
     });
 
     it('should change depth', () => {
@@ -25,7 +25,7 @@ describe('Paper.getStyles', () => {
       // I'm now also testing combineStyles,
       // this can be avoided by exporting all internal functions as one object...
       expect(style).to.be.an('object');
-      expect(style).to.have.all.keys('padding', 'borderRadius', 'boxShadow', 'background', 'margin', 'color');
+      expect(style).to.have.all.keys('padding', 'borderRadius', 'boxShadow', 'backgroundColor', 'margin', 'color');
     });
   });
 });
