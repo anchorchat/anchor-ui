bfe611abd1b72c84a3b0aadf5309152567a9b481
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-07-28 17:43:04 +0200

    Added key events to Commands (#818)
    * Added onMenuClose.
    
    * Changed styles.
    
    * Changed onMouseOver to onChange.
    
    * Changed avatar styles.
    
    * Added key events.
    
    * Wip fixing docs.
    
    * WIP docs.
    
    * Removed borderRadius override.
    
    * Fixed docs.
    
    * Return this.hideMenu();
    
    * Changed to single line.
    
    * Added onMenuOpen.

--- src/commands/get-styles.js
+++ src/commands/get-styles.js
@@ -3,15 +3,19 @@ import combineStyles from '../internal/combine-styles';
 import colors from '../settings/colors';
 
 const root = overrideStyle => combineStyles(styles.root, overrideStyle);
+const header = overrideStyle => combineStyles(styles.header, overrideStyle);
+const commands = overrideStyle => combineStyles(styles.commands, overrideStyle);
 
-const header = (color = colors.theme, overrideStyle) => {
-  const style = combineStyles(styles.header, { backgroundColor: color });
+const command = (color = colors.theme, active, overrideStyle) => {
+  let style = styles.command;
+
+  if (active) {
+    style = combineStyles(style, { backgroundColor: color, color: colors.white });
+  }
 
   return combineStyles(style, overrideStyle);
 };
 
-const commands = overrideStyle => combineStyles(styles.commands, overrideStyle);
-const command = overrideStyle => combineStyles(styles.command, overrideStyle);
 const title = overrideStyle => combineStyles(styles.title, overrideStyle);
 const description = overrideStyle => combineStyles(styles.description, overrideStyle);
 
