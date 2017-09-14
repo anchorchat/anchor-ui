e7ea6c86ce61aa1183927de9a5b10c0d2d05f945
Ian Stewart <IanCStewart@users.noreply.github.com>
2017-09-14 15:51:39 +0200

    Issue #859 (#860)
    * Added fallaback for avatar.
    
    * DocGen. Fixes #859.
    
    * Fixed tests.
    
    * DocGen
    
    * Added what it does to docs line.
    
    * Adjusted to path intead.

--- src/avatar/get-styles.js
+++ src/avatar/get-styles.js
@@ -2,8 +2,12 @@ import styles from './styles';
 import combineStyles from '../internal/combine-styles';
 import colors from '../settings/colors';
 
-const root = (image, overrideStyle) => {
-  const style = combineStyles(styles.root, { backgroundImage: `url(${image})` });
+const root = (image, defaultImage = null, overrideStyle) => {
+  let style = combineStyles(styles.root, { backgroundImage: `url(${image})` });
+
+  if (defaultImage) {
+    style = combineStyles(style, { backgroundImage: `url(${image}), url(${defaultImage})` });
+  }
 
   return combineStyles(style, overrideStyle);
 };
--- src/avatar/index.jsx
+++ src/avatar/index.jsx
@@ -5,8 +5,8 @@ import Radium from 'radium';
 import getStyles from './get-styles';
 
 /** An user's profile image */
-const Avatar = ({ image, style, status, statusStyle, ...custom }) => (
-  <section style={getStyles.root(image, style)} {...custom}>
+const Avatar = ({ image, defaultImage, style, status, statusStyle, ...custom }) => (
+  <section style={getStyles.root(image, defaultImage, style)} {...custom}>
     {status ? <div style={getStyles.status(status, statusStyle)} /> : null}
   </section>
 );
@@ -16,6 +16,8 @@ Avatar.displayName = 'Avatar';
 Avatar.propTypes = {
   /** Path to user's profile image */
   image: PropTypes.string.isRequired,
+  /** Path to default image. This image will be shown if the image path doesn't resolve */
+  defaultImage: PropTypes.string,
   /** Override the styles of the root element */
   style: PropTypes.instanceOf(Object),
   /** An user's status. One of the following: ["online", "away", "offline"] */
@@ -27,7 +29,8 @@ Avatar.propTypes = {
 Avatar.defaultProps = {
   style: {},
   status: '',
-  statusStyle: {}
+  statusStyle: {},
+  defaultImage: null
 };
 
 export default pure(Radium(Avatar));
