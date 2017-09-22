da8e9ddb6794ba36725358407148aa287a151ba7
iancstewart <ian.c.stewart@icloud.com>
2017-09-22 09:43:51 +0200

    Fixed imports for TravisCI.

--- src/admin-badge/component.jsx
+++ src/admin-badge/component.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import pure from 'recompose/pure';
-import Radium from 'radium';
-import compose from 'recompose/compose';
 import getStyles from './get-styles';
-import themeable from '../themeable';
 
 /** Used for displaying an user's Admin status */
 class AdminBadge extends Component {
