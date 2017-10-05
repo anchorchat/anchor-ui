f5977914a2bf34949b59f8423f63a41cd0eda829
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-10-05 16:28:37 +0200

    Added ligthbox to docs.

--- docs/src/components/gallery.jsx
+++ docs/src/components/gallery.jsx
@@ -1,80 +1,119 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import _ from 'lodash';
 import Gallery from '../../../dist/gallery';
 import Props from './props';
 import components from '../../components.json';
 import Paper from '../../../dist/paper';
+import Select from '../../../dist/select';
+import MenuItem from '../../../dist/menu-item';
 
 const usage = '```js\n import Gallery from \'anchor-ui/gallery\';';
 
-const GalleryDoc = () => {
-  const componentData = _.find(components, component => component.displayName === 'Gallery');
+const images = [
+  {
+    src: 'https://source.unsplash.com/featured/?man',
+    alt: 'https://source.unsplash.com/featured/?man'
+  },
+  {
+    src: 'https://source.unsplash.com/featured/?woman',
+    alt: 'https://source.unsplash.com/featured/?woman'
+  },
+  {
+    src: 'https://source.unsplash.com/featured/?nature',
+    alt: 'https://source.unsplash.com/featured/?nature'
+  },
+  {
+    src: 'https://source.unsplash.com/featured/?portrait',
+    alt: 'https://source.unsplash.com/featured/?portrait'
+  },
+  {
+    src: 'https://source.unsplash.com/featured/?office',
+    alt: 'https://source.unsplash.com/featured/?office'
+  },
+  {
+    src: 'https://source.unsplash.com/featured/?design',
+    alt: 'https://source.unsplash.com/featured/?design'
+  },
+  {
+    src: 'https://source.unsplash.com/featured/?tree',
+    alt: 'https://source.unsplash.com/featured/?tree'
+  }
+];
 
-  const style = {
-    paper: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      margin: 0,
-      padding: '20px'
-    }
-  };
+class GalleryDoc extends Component {
+  constructor() {
+    super();
 
-  const images = [
-    {
-      src: 'https://source.unsplash.com/featured/?man',
-      alt: 'https://source.unsplash.com/featured/?man'
-    },
-    {
-      src: 'https://source.unsplash.com/featured/?woman',
-      alt: 'https://source.unsplash.com/featured/?woman'
-    },
-    {
-      src: 'https://source.unsplash.com/featured/?nature',
-      alt: 'https://source.unsplash.com/featured/?nature'
-    },
-    {
-      src: 'https://source.unsplash.com/featured/?portrait',
-      alt: 'https://source.unsplash.com/featured/?portrait'
-    },
-    {
-      src: 'https://source.unsplash.com/featured/?office',
-      alt: 'https://source.unsplash.com/featured/?office'
-    },
-    {
-      src: 'https://source.unsplash.com/featured/?design',
-      alt: 'https://source.unsplash.com/featured/?design'
-    },
-    {
-      src: 'https://source.unsplash.com/featured/?tree',
-      alt: 'https://source.unsplash.com/featured/?tree'
-    }
-  ];
+    this.state = {
+      lightbox: false,
+      itemHeight: 320
+    };
+  }
 
-  return (
-    <article className="doc">
-      <h1>Gallery</h1>
-      <section>
-        <h1>Description</h1>
-        <p>{componentData.description}</p>
-      </section>
-      <section>
-        <h1>Usage</h1>
-        <ReactMarkdown source={usage} className="markdown" />
-      </section>
-      <section>
-        <h1>Examples</h1>
-        <Paper style={style.paper}>
-          <h2>Default</h2>
-          <Gallery items={images} />
-          <h2>With itemHeight</h2>
-          <Gallery items={images} itemHeight={240} />
-        </Paper>
-      </section>
-      <Props props={componentData.props} />
-    </article>
-  );
-};
+  selectItemHeight = (event, itemHeight) => this.setState({ itemHeight })
+
+  selectLightbox = (event, lightbox) => this.setState({ lightbox })
+
+  render() {
+    const componentData = _.find(components, component => component.displayName === 'Gallery');
+
+    const style = {
+      paper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        margin: 0,
+        padding: '20px'
+      },
+      options: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        padding: '10px 0'
+      },
+      select: {
+        margin: '5px'
+      }
+    };
+
+    return (
+      <article className="doc">
+        <h1>Gallery</h1>
+        <section>
+          <h1>Description</h1>
+          <p>{componentData.description}</p>
+        </section>
+        <section>
+          <h1>Usage</h1>
+          <ReactMarkdown source={usage} className="markdown" />
+        </section>
+        <section>
+          <h1>Examples</h1>
+          <h1>Examples</h1>
+          <div style={style.options}>
+            <Select style={style.select} label="Lightbox" value={this.state.lightbox} onChange={this.selectLightbox}>
+              <MenuItem text="On" value />
+              <MenuItem text="Off" value={false} />
+            </Select>
+            <Select style={style.select} label="Item height" value={this.state.itemHeight} onChange={this.selectItemHeight}>
+              <MenuItem text="400" value={400} />
+              <MenuItem text="320" value={320} />
+              <MenuItem text="240" value={240} />
+              <MenuItem text="120" value={120} />
+            </Select>
+          </div>
+          <Paper style={style.paper}>
+            <Gallery
+              items={images}
+              itemHeight={this.state.itemHeight}
+              enableLightbox={this.state.lightbox}
+            />
+          </Paper>
+        </section>
+        <Props props={componentData.props} />
+      </article>
+    );
+  }
+}
 
 export default GalleryDoc;
