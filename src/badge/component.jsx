9f27e38835ebb4993b61044401d4c71a4d5221ee
iancstewart <ian.c.stewart@icloud.com>
2017-10-11 16:49:09 +0200

    New structure for correct testing.

--- src/badge/component.jsx
+++ src/badge/component.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Radium from 'radium';
-import compose from 'recompose/compose';
 import isNumber from 'lodash/isNumber';
 import getStyles from './get-styles';
-import themeable from '../themeable';
 
 /** Used for displaying a (notification) counter */
 const Badge = ({ value, maxValue, inverted, style, color, ...custom }) => {
@@ -43,9 +40,4 @@ Badge.defaultProps = {
   maxValue: null
 };
 
-const enhance = compose(
-  themeable(),
-  Radium
-);
-
-export default enhance(Badge);
+export default Badge;
