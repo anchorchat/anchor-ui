0dd477b412bece90dfad5a20052a9026443f9160
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-10-31 09:19:14 +0100

    Added tests.

--- test/radio-button-group/get-styles.test.js
+++ test/radio-button-group/get-styles.test.js
@@ -4,6 +4,20 @@ import getStyles from '../../src/radio-button-group/get-styles';
 import styles from '../../src/radio-button-group/styles';
 
 describe('RadioButtonGroup.getStyles', () => {
+  describe('buttons', () => {
+    it('should get styles', () => {
+      const style = getStyles.buttons();
+
+      expect(style).to.deep.equal(styles.buttons);
+    });
+
+    it('should combine styles', () => {
+      const style = getStyles.buttons({ color: 'red' });
+
+      expect(style).to.have.property('color', 'red');
+    });
+  });
+
   describe('label', () => {
     it('should get styles', () => {
       const style = getStyles.label();
@@ -13,6 +27,20 @@ describe('RadioButtonGroup.getStyles', () => {
 
     it('should combine styles', () => {
       const style = getStyles.label({ color: 'red' });
+
+      expect(style).to.have.property('color', 'red');
+    });
+  });
+
+  describe('error', () => {
+    it('should get styles', () => {
+      const style = getStyles.error();
+
+      expect(style).to.deep.equal(styles.error);
+    });
+
+    it('should combine styles', () => {
+      const style = getStyles.error({ color: 'red' });
 
       expect(style).to.have.property('color', 'red');
     });
