0a59923b9bf7536f8d7207503650ad03390a4756
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-11-03 19:16:18 +0100

    Changed classname.

--- docs/src/components/app.jsx
+++ docs/src/components/app.jsx
@@ -57,7 +57,7 @@ class App extends Component {
               </a>
             }
           />
-          <article className="doc">
+          <article className="docs">
             <Nav />
             {childrenWithProps}
           </article>
