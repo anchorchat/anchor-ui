156252cfa81e3f30a097aa0ece071992fd6dbef2
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-11-04 10:24:24 +0100

    Added example.

--- docs/src/components/badge/index.jsx
+++ docs/src/components/badge/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import _ from 'lodash';
-import Badge from '../../../dist/badge';
-import Props from './props';
-import components from '../../components.json';
-import Paper from '../../../dist/paper';
-import Markdown from './markdown';
+import Badge from '../../../../dist/badge';
+import Props from '../props';
+import components from '../../../components.json';
+import Paper from '../../../../dist/paper';
+import Markdown from '../markdown';
 
 const usage = `
-  \`\`\`js
+  \`\`\`jsx
+  import React from 'react';
   import Badge from 'anchor-ui/badge';
+
+  const BadgeExample = () => (
+    <section>
+      <Badge value={9} maxValue={9} />
+      <Badge value={10} maxValue={9} />
+      <Badge value={10} maxValue={9} inverted />
+      <Badge value={100} />
+      <Badge value={100} maxValue={99} />
+      <Badge value={1000} />
+    </section>
+  );
+
+  export default BadgeExample;
   \`\`\`
 `;
 
@@ -32,9 +46,9 @@ const BadgeDoc = () => {
         <h1>Description</h1>
         <p>{componentData.description}</p>
       </section>
-      <Markdown markdown={usage} title="Code example" />
       <section>
         <h1>Examples</h1>
+        <Markdown markdown={usage} title="Code example" />
         <Paper style={style.paper}>
           <Badge style={style.badge} value={9} maxValue={9} />
           <Badge style={style.badge} value={10} maxValue={9} />
