48266bc2538d4a5eca07cfbdf319a5c691c22340
iancstewart <ian.c.stewart@icloud.com>
2017-11-10 15:40:56 +0100

    Fixed index test.

--- test/channel-header/index.test.js
+++ test/channel-header/index.test.js
@@ -5,7 +5,7 @@ import chai, { expect } from 'chai';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import ChannelHeader from '../../src/channel-header';
+import ChannelHeader from '../../src/channel-header/component';
 import getStyles from '../../src/channel-header/get-styles';
 
 chai.use(sinonChai);
@@ -15,10 +15,10 @@ describe('ChannelHeader', () => {
     name: 'name',
     rightButton: null,
     leftButton: null,
-    style: { root: true },
-    textStyle: { text: true },
-    rightButtonStyle: { rightButton: true },
-    leftButtonStyle: { leftButton: true }
+    style: {},
+    textStyle: {},
+    rightButtonStyle: {},
+    leftButtonStyle: {}
   };
 
   beforeEach(() => {
@@ -29,64 +29,62 @@ describe('ChannelHeader', () => {
     global.navigator = undefined;
   });
 
-  it('should always render a header element', () => {
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
+  it('should render a header element containing a h1 element', () => {
+    const component = shallow(<ChannelHeader {...props} />);
 
-    expect(wrapper.find('header')).to.have.length(1);
+    expect(component.find('header')).to.have.length(1);
+    expect(component.find('h1')).to.have.length(1);
   });
 
-  it('should always render a h1 element', () => {
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
+  it('should render leftButton', () => {
+    const component = shallow(<ChannelHeader {...props} />);
 
-    expect(wrapper.find('h1')).to.have.length(1);
+    expect(component.find('div')).to.have.length(0);
+
+    component.setProps({ leftButton: <button /> });
+    expect(component.find('div')).to.have.length(1);
   });
 
-  it('should not render a div element if the leftButton prop is not passed', () => {
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
+  it('should render rightButton', () => {
+    const component = shallow(<ChannelHeader {...props} />);
 
-    expect(wrapper.find('div')).to.have.length(0);
+    expect(component.find('div')).to.have.length(0);
+
+    component.setProps({ rightButton: <button /> });
+    expect(component.find('div')).to.have.length(1);
   });
 
-  it('should render a div element if the leftButton prop is passed', () => {
-    props.leftButton = <button />;
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
+  it('should render name', () => {
+    const component = shallow(<ChannelHeader {...props} />);
 
-    expect(wrapper.containsMatchingElement(<div><button /></div>)).to.equal(true);
-    props.leftButton = null;
+    expect(component.containsMatchingElement(<h1>name</h1>)).to.equal(true);
   });
 
-  it('should not render a div element if the rightButton prop is not passed', () => {
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
+  it('should render secondaryText', () => {
+    const component = shallow(<ChannelHeader {...props} />);
 
-    expect(wrapper.find('div')).to.have.length(0);
-  });
+    expect(component.find('h2')).to.have.length(0);
 
-  it('should render a div element if the rightButton prop is passed', () => {
-    props.rightButton = <button />;
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
-
-    expect(wrapper.containsMatchingElement(<div><button /></div>)).to.equal(true);
-    props.rightButton = null;
-  });
-
-  it('should pass the name prop to the h1 element', () => {
-    const wrapper = shallow(<ChannelHeader {...props} />).dive();
-
-    expect(wrapper.containsMatchingElement(<h1>name</h1>)).to.equal(true);
+    component.setProps({ secondaryText: 'secondaryText' });
+    expect(component.find('h2')).to.have.length(1);
+    expect(component.containsMatchingElement(<h2>secondaryText</h2>)).to.equal(true);
   });
 
   it('should get root styles', () => {
     const spy = sinon.spy(getStyles, 'root');
 
-    shallow(<ChannelHeader {...props} />).dive();
+    shallow(<ChannelHeader {...props} />);
     expect(spy).to.have.been.calledWith(props.style);
   });
 
   it('should get leftButton styles', () => {
-    props.leftButton = <button />;
     const spy = sinon.spy(getStyles, 'leftButton');
+    const combinedProps = {
+      ...props,
+      leftButton: <button />
+    };
 
-    shallow(<ChannelHeader {...props} />).dive();
+    shallow(<ChannelHeader {...combinedProps} />);
     expect(spy).to.have.been.calledWith(props.leftButtonStyle);
     props.leftButton = null;
   });
@@ -94,15 +92,29 @@ describe('ChannelHeader', () => {
   it('should get text styles', () => {
     const spy = sinon.spy(getStyles, 'text');
 
-    shallow(<ChannelHeader {...props} />).dive();
+    shallow(<ChannelHeader {...props} />);
     expect(spy).to.have.been.calledWith(props.textStyle);
   });
 
-  it('should get rightButton styles', () => {
-    props.rightButton = <button />;
-    const spy = sinon.spy(getStyles, 'rightButton');
+  it('should get secondaryText styles', () => {
+    const spy = sinon.spy(getStyles, 'secondaryText');
+    const combinedProps = {
+      ...props,
+      secondaryText: 'text'
+    };
 
-    shallow(<ChannelHeader {...props} />).dive();
+    shallow(<ChannelHeader {...combinedProps} />);
+    expect(spy).to.have.been.calledWith(props.secondaryTextStyle);
+  });
+
+  it('should get rightButton styles', () => {
+    const spy = sinon.spy(getStyles, 'rightButton');
+    const combinedProps = {
+      ...props,
+      rightButton: <button />
+    };
+
+    shallow(<ChannelHeader {...combinedProps} />);
     expect(spy).to.have.been.calledWith(props.rightButtonStyle);
     props.rightButton = null;
   });
