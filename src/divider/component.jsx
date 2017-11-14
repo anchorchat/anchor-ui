b8b76b37c59b2014dd20b93755d18bac913d7b84
Ian Stewart <IanCStewart@users.noreply.github.com>
2017-11-14 11:33:14 +0100

    Issue #552 divider (#976)
    * Refactored to es6 functions.
    
    * New structure for correct testing.
    
    * Fixed index test for Divider.
    
    * DocGen.

---
+++ src/divider/component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import getStyles from './get-styles';
+
+const propTypes = {
+  /** Optional divider text */
+  text: PropTypes.node,
+  /** Override the styles of the root element */
+  style: PropTypes.instanceOf(Object)
+};
+
+const defaultProps = {
+  text: null,
+  style: {}
+};
+
+const displayName = 'Divider';
+
+/** Dividers group and separate content within lists and page layouts. */
+const Divider = ({ text, style, ...custom }) => {
+  if (!text) {
+    return <hr style={getStyles.hr(style)} {...custom} />;
+  }
+
+  return <h1 style={getStyles.text(style)} {...custom}>{text}</h1>;
+};
+
+Divider.propTypes = propTypes;
+Divider.defaultProps = defaultProps;
+Divider.displayName = displayName;
+
+export default Divider;
