82cc7c3cda7bd1e10f16e62a2e8976fc59faa08a
Ian Stewart <IanCStewart@users.noreply.github.com>
2017-11-14 10:50:40 +0100

    Fixed badge uncovered lines. (#975)
    * Fixed badge uncovered lines.
    
    * Fixed broken test.

--- test/badge/get-styles.test.js
+++ test/badge/get-styles.test.js
@@ -1,14 +1,14 @@
 /* eslint-env mocha */
 import { expect } from 'chai';
-import getStyles from '../../src/admin-badge/get-styles';
-import styles from '../../src/admin-badge/styles';
+import getStyles from '../../src/badge/get-styles';
+import styles from '../../src/badge/styles';
 
 describe('Badge.getStyles', () => {
   describe('root', () => {
     it('should get styles', () => {
       const style = getStyles.root();
 
-      expect(style).to.deep.equal(styles.root);
+      expect(style).to.deep.equal(styles.badge);
     });
 
     it('should combine styles', () => {
@@ -24,10 +24,9 @@ describe('Badge.getStyles', () => {
     });
 
     it('should add inverted styles', () => {
-      const style = getStyles.root('red', true);
+      const style = getStyles.root(undefined, true);
 
-      expect(style).to.have.property('backgroundColor', '#FEFEFE');
-      expect(style).to.have.property('color', 'red');
+      expect(style).to.include({ ...style.inverted });
     });
   });
 });
