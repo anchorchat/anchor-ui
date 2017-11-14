38d8fc773156c6a27008ec2ea5145b6014bff981
iancstewart <ian.c.stewart@icloud.com>
2017-11-14 10:51:18 +0100

    Refactored to es6 functions.

--- src/banner/get-styles.js
+++ src/banner/get-styles.js
@@ -1,9 +1,9 @@
 import styles from './styles';
 import combineStyles from '../internal/combine-styles';
 
-function root(type, overrideStyle) {
-  return combineStyles(combineStyles(styles.root, styles[type]), overrideStyle);
-}
+const root = (type, overrideStyle) => (
+  combineStyles(combineStyles(styles.root, styles[type]), overrideStyle)
+);
 
 export default {
   root
