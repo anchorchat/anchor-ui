2f4f31dc62788c691b88282b41a29c833f1f4aad
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-11-22 08:56:46 +0100

    Fixed linting errors.

--- src/admin-badge/component.jsx
+++ src/admin-badge/component.jsx
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import getStyles from './get-styles';
 
-/** Used for displaying an user's Admin status */
-class AdminBadge extends Component {
-  static displayName = 'AdminBadge'
+const displayName = 'AdminBadge';
 
-  static propTypes = {
-    /** The badge's text */
-    text: PropTypes.node,
-    /** Override the styles of the root element */
-    style: PropTypes.instanceOf(Object),
-    /** Inverts color */
-    inverted: PropTypes.bool,
-    color: PropTypes.string.isRequired
-  }
+const propTypes = {
+  /** The badge's text */
+  text: PropTypes.node,
+  /** Override the styles of the root element */
+  style: PropTypes.instanceOf(Object),
+  /** Inverts color */
+  inverted: PropTypes.bool,
+  color: PropTypes.string.isRequired
+};
 
-  static defaultProps = {
-    style: {},
-    inverted: false,
-    text: 'Admin'
-  }
+const defaultProps = {
+  style: {},
+  inverted: false,
+  text: 'Admin'
+};
 
-  static contextTypes = {
-    color: PropTypes.string
-  }
+/** Used for displaying an user's Admin role */
+const AdminBadge = ({
+  text, inverted, style, color, ...custom
+}) => (
+  <span style={getStyles.root(color, inverted, style)} {...custom}>
+    {text}
+  </span>
+);
 
-  render() {
-    const { text, inverted, style, color, ...custom } = this.props;
-
-    return (
-      <span style={getStyles.root(color, inverted, style)} {...custom}>
-        {text}
-      </span>
-    );
-  }
-}
+AdminBadge.displayName = displayName;
+AdminBadge.propTypes = propTypes;
+AdminBadge.defaultProps = defaultProps;
 
 export default AdminBadge;
