7d692f1e1e60446545ac59538e63448d8553a304
Mitchel van Bever <mitchelvanbever@hotmail.com>
2017-11-28 10:56:59 +0100

    added comment

--- src/internal/get-percentage.js
+++ src/internal/get-percentage.js
@@ -1,3 +1,7 @@
+// date: 28-11 '17
+// This function used to make use of lodash isNumber()
+// however this doesn't work as expected
+// Should remove isNaN when isNumber works as intended
 const getPercentage = (value, min, max) => {
   let percentage = (value - min) / (max - min);
 
