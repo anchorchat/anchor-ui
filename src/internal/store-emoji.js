ea208c5d9321f90f228b84cc67cd1418d9277002
iancstewart <ian.c.stewart@icloud.com>
2018-01-02 16:30:06 +0100

    Cleaned the campgrounds.

--- src/internal/store-emoji.js
+++ src/internal/store-emoji.js
@@ -2,38 +2,13 @@ import _ from 'lodash';
 import store from 'store';
 
 const storeEmoji = (emoji) => {
-  const key = 'recent-emoji';
-  const storedEmojis = store.get(key);
-  let value;
+  let storedEmojis = store.get('recent-emoji');
 
-  if (
-    storedEmojis &&
-    _.find(storedEmojis, storedEmoji => storedEmoji.shortname === emoji.shortname)
-  ) {
-    _.remove(storedEmojis, storedEmoji => storedEmoji.shortname === emoji.shortname);
-    storedEmojis.unshift(emoji);
-    value = storedEmojis;
+  if (!_.isArray(storedEmojis)) {
+    storedEmojis = [];
   }
 
-  if (!storedEmojis) {
-    value = [emoji];
-  }
-
-  if (
-    storedEmojis &&
-    !_.find(storedEmojis, storedEmoji => storedEmoji.shortname === emoji.shortname)
-  ) {
-    storedEmojis.unshift(emoji);
-    value = storedEmojis;
-  }
-
-  if (value.length > 42) {
-    value.pop();
-  }
-
-  store.set(key, value);
-
-  return false;
+  store.set('recent-emoji', _.take(_.unionBy([emoji], storedEmojis, 'shortname'), 42));
 };
 
 export default storeEmoji;
