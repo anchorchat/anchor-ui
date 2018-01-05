c615ff804df9348683d956a44db414dfe9c64ea4
iancstewart <ian.c.stewart@icloud.com>
2018-01-05 15:01:11 +0100

    New structure for correct testing.

--- src/input/component.jsx
+++ src/input/component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Radium, { Style } from 'radium';
+import { Style } from 'radium';
 import getStyles from './get-styles';
 
 const propTypes = {
@@ -177,4 +177,4 @@ Input.displayName = displayName;
 Input.propTypes = propTypes;
 Input.defaultProps = defaultProps;
 
-export default Radium(Input);
+export default Input;
