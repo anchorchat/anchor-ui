483eee6b32fc4bb2e7860485cb03d926e364acef
iancstewart <ian.c.stewart@icloud.com>
2018-01-08 16:41:26 +0100

    New structure for correct testing.

--- src/list-item/component.jsx
+++ src/list-item/component.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Radium from 'radium';
-import compose from 'recompose/compose';
 import styles from './styles';
 import colors from '../settings/colors';
 import Avatar from '../avatar';
@@ -11,7 +9,6 @@ import IconChevronDown from '../icons/icon-chevron-down';
 import getStyles from './get-styles';
 import Button from '../button';
 import List from '../list';
-import themeable from '../themeable';
 
 const displayName = 'ListItem';
 
@@ -212,9 +209,4 @@ ListItem.displayName = displayName;
 ListItem.propTypes = propTypes;
 ListItem.defaultProps = defaultProps;
 
-const enhance = compose(
-  themeable(),
-  Radium
-);
-
-export default enhance(ListItem);
+export default ListItem;
