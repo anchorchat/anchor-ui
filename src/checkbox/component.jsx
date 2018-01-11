725c08fc4f2e7ca4d9de3b3fa6f527838bda2a33
iancstewart <ian.c.stewart@icloud.com>
2018-01-11 17:17:38 +0100

    Added custom style possibilities to unchecked element.

--- src/checkbox/component.jsx
+++ src/checkbox/component.jsx
@@ -2,37 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getStyles from './get-styles';
 import IconCheckbox from '../icons/icon-checkbox';
-import styles from './styles';
 
-/** A checkbox is used to verify which options you want selected from a group. */
-const Checkbox = ({
-  onChange, label, checked, name, style, inputStyle, iconStyle, labelStyle, value, color, ...custom
-}) => (
-  <label htmlFor={name} style={getStyles.root(color, style)} {...custom}>
-    <input
-      type="checkbox"
-      id={name}
-      onChange={onChange}
-      checked={checked}
-      style={getStyles.input(inputStyle)}
-      value={value}
-    />
-    <div style={getStyles.icon(iconStyle)}>
-      {
-        checked
-        ? <IconCheckbox color={color} />
-        : <div style={styles.unchecked} />
-      }
-    </div>
-    <span style={getStyles.label(labelStyle)}>
-      {label}
-    </span>
-  </label>
-);
-
-Checkbox.displayName = 'Checkbox';
-
-Checkbox.propTypes = {
+const propTypes = {
   /** The input's label text */
   label: PropTypes.node.isRequired,
   /** The input's name */
@@ -53,17 +24,52 @@ Checkbox.propTypes = {
   iconStyle: PropTypes.instanceOf(Object),
   /** Override the styles of the label element */
   labelStyle: PropTypes.instanceOf(Object),
+  /** Override the styles of the unchecked element */
+  uncheckedStyles: PropTypes.instanceOf(Object),
   /** The checkbox' value */
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]).isRequired,
   color: PropTypes.string.isRequired
 };
 
-Checkbox.defaultProps = {
+const defaultProps = {
   style: {},
   inputStyle: {},
   iconStyle: {},
   labelStyle: {},
+  uncheckedStyles: {},
   checked: false
 };
+
+const displayName = 'Checkbox';
+
+/** A checkbox is used to verify which options you want selected from a group. */
+const Checkbox = ({
+  onChange, label, checked, name, style, inputStyle, iconStyle, labelStyle, uncheckedStyles, value, color, ...custom // eslint-disable-line max-len
+}) => (
+  <label htmlFor={name} style={getStyles.root(color, style)} {...custom}>
+    <input
+      type="checkbox"
+      id={name}
+      onChange={onChange}
+      checked={checked}
+      style={getStyles.input(inputStyle)}
+      value={value}
+    />
+    <div style={getStyles.icon(iconStyle)}>
+      {
+        checked
+        ? <IconCheckbox color={color} />
+        : <div style={getStyles.unchecked(uncheckedStyles)} />
+      }
+    </div>
+    <span style={getStyles.label(labelStyle)}>
+      {label}
+    </span>
+  </label>
+);
+
+Checkbox.propTypes = propTypes;
+Checkbox.defaultProps = defaultProps;
+Checkbox.displayName = displayName;
 
 export default Checkbox;
