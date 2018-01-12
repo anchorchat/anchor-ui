8c573e50f8e3e4c872cc9d6b72f414debf94e16b
iancstewart <ian.c.stewart@icloud.com>
2018-01-12 09:58:25 +0100

    Changed code styling of radio-group file.

--- src/radio-button-group/index.jsx
+++ src/radio-button-group/index.jsx
@@ -3,6 +3,42 @@ import PropTypes from 'prop-types';
 import Radium from 'radium';
 import getStyles from './get-styles';
 
+const propTypes = {
+  /** The RadioButtonGroup's value */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  /**
+   * Callback fired when RadioButtonGroup's value changes
+   *
+   * function(event: object) => void
+   */
+  onChange: PropTypes.func.isRequired,
+  /** The RadioButtonGroup's content (RadioButton), each child must have a value prop */
+  children: PropTypes.node.isRequired,
+  /** The RadioButtonGroup's label */
+  label: PropTypes.node,
+  /** Override the styles of the label element */
+  labelStyle: PropTypes.instanceOf(Object),
+  /** Override the styles of the root element */
+  style: PropTypes.instanceOf(Object),
+  /** Override the styles of the button container */
+  buttonStyle: PropTypes.instanceOf(Object),
+  /** Display an error message */
+  error: PropTypes.node,
+  /** Override the styles of the error element */
+  errorStyle: PropTypes.instanceOf(Object)
+};
+
+const defaultProps = {
+  label: null,
+  labelStyle: {},
+  style: {},
+  error: null,
+  errorStyle: {},
+  buttonStyle: {}
+};
+
+const displayName = 'RadioButtonGroup';
+
 const RadioButtonGroup = ({
   value, onChange, children, buttonStyle, label, labelStyle, style, error, errorStyle, ...custom
 }) => {
@@ -28,40 +64,8 @@ const RadioButtonGroup = ({
   );
 };
 
-RadioButtonGroup.propTypes = {
-  /** The RadioButtonGroup's value */
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  /**
-   * Callback fired when RadioButtonGroup's value changes
-   *
-   * function(event: object) => void
-   */
-  onChange: PropTypes.func.isRequired,
-  /** The RadioButtonGroup's content (RadioButton), each child must have a value prop */
-  children: PropTypes.node.isRequired,
-  /** The RadioButtonGroup's label */
-  label: PropTypes.node,
-  /** Override the styles of the label element */
-  labelStyle: PropTypes.instanceOf(Object),
-  /** Override the styles of the root element */
-  style: PropTypes.instanceOf(Object),
-  /** Override the styles of the button container */
-  buttonStyle: PropTypes.instanceOf(Object),
-  /** Display an error message */
-  error: PropTypes.node,
-  /** Override the styles of the error element */
-  errorStyle: PropTypes.instanceOf(Object)
-};
-
-RadioButtonGroup.defaultProps = {
-  label: null,
-  labelStyle: {},
-  style: {},
-  error: null,
-  errorStyle: {},
-  buttonStyle: {}
-};
-
-RadioButtonGroup.displayName = 'RadioButtonGroup';
+RadioButtonGroup.propTypes = propTypes;
+RadioButtonGroup.defaultProps = defaultProps;
+RadioButtonGroup.displayName = displayName;
 
 export default Radium(RadioButtonGroup);
