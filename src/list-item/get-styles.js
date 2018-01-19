6760810b73b1204b963ccc5d9f72305f08abcb4e
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-01-19 09:49:26 +0100

    Removed nestedList argument.

--- src/list-item/get-styles.js
+++ src/list-item/get-styles.js
@@ -9,12 +9,9 @@ const root = (
   rightButton,
   avatar,
   secondaryLine = null,
-  nestedLevel,
   overrideStyle
 ) => {
   let style = styles.root;
-
-  style = combineStyles(style, { paddingLeft: `${(nestedLevel * 16) + 8}px` });
 
   const activeStyle = combineStyles(
     styles.root,
