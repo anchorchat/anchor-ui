6102e81ab84faf7630b19f424be131298618c579
iancstewart <ian.c.stewart@icloud.com>
2018-02-27 12:28:55 +0100

    Fixed tests.

--- test/message/image-message-get-styles.test.js
+++ test/message/image-message-get-styles.test.js
@@ -57,7 +57,7 @@ describe('ImageMessage.getStyles', () => {
     it('should add collapsed styles', () => {
       const style = getStyles.root('red', false, false, false, true, true);
 
-      expect(style).to.have.property('padding', '12px 40px 12px 12px');
+      expect(style).to.have.property('padding', '13px 48px 13px 13px');
     });
   });
 
