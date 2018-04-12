8001c84082b842cb07a33b3010a2223da6af7706
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-04-12 16:19:22 +0200

    Changed code styling.

--- src/with-theme/index.jsx
+++ src/with-theme/index.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ThemeContext from '../internal/theme-context';
 
 const withTheme = Component => (
-  function ThemedComponent(props) {
-    return (
-      <ThemeContext.Consumer>
-        {color => <Component {...props} color={color} />}
-      </ThemeContext.Consumer>
-    );
-  }
+  props => (
+    <ThemeContext.Consumer>
+      {color => <Component {...props} color={color} />}
+    </ThemeContext.Consumer>
+  )
 );
 
 export default withTheme;
