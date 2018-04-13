e1d7ca53d5dbf056651acf4b624732b54ac24ee3
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-04-13 09:14:29 +0200

    Export component separately.

--- src/switch/component.jsx
+++ src/switch/component.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Radium from 'radium';
-import compose from 'recompose/compose';
 import styles from './styles';
 import getStyles from './get-styles';
-import withTheme from '../with-theme';
 
 const propTypes = {
   /** The Switch's active state */
@@ -55,9 +52,4 @@ Switch.propTypes = propTypes;
 Switch.defaultProps = defaultProps;
 Switch.displayName = displayName;
 
-const enhance = compose(
-  withTheme,
-  Radium
-);
-
-export default enhance(Switch);
+export default Switch;
