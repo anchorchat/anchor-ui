f346da81f6442960bb954e1e22c9b13f37749e44
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-04-13 09:16:12 +0200

    Export component separately.

--- src/tab/component.jsx
+++ src/tab/component.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Radium from 'radium';
-import compose from 'recompose/compose';
 import getStyles from './get-styles';
-import withTheme from '../with-theme';
 
 const propTypes = {
   /** The Tab's icon */
@@ -79,9 +76,4 @@ Tab.propTypes = propTypes;
 Tab.defaultProps = defaultProps;
 Tab.displayName = displayName;
 
-const enhance = compose(
-  withTheme,
-  Radium
-);
-
-export default enhance(Tab);
+export default Tab;
