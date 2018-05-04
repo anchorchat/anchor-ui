7818aa0bf23a36a8902dd0602b7e4625fc67f854
iancstewart <ian.c.stewart@icloud.com>
2018-05-04 13:39:51 +0200

    WIP fixing index test.

--- test/menu/index.test.js
+++ test/menu/index.test.js
@@ -1,13 +1,14 @@
 /* eslint-env mocha */
 /* eslint react/jsx-filename-extension: [0] */
-import React, { cloneElement } from 'react';
+import React from 'react';
 import chai, { expect } from 'chai';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import isFunction from 'lodash/isFunction';
 import noop from 'lodash/noop';
-import Menu from '../../src/menu';
+import Menu from '../../src/menu/component';
+import MenuItem from '../../src/menu-item';
 import Overlay from '../../src/overlay';
 import getStyles from '../../src/menu/get-styles';
 
@@ -17,17 +18,14 @@ describe('Menu', () => {
   const props = {
     open: false,
     closeMenu: null,
-    header: '',
+    header: undefined,
     headerIcon: null,
     iconStyle: {},
     style: {},
     headerStyle: {},
-    color: '#1BA6C4'
+    color: '#1BA6C4',
+    children: <MenuItem text="Menu item" onClick={noop} />
   };
-  const children = <p>children</p>;
-  const menuItems = React.Children.map(children, child => (
-    cloneElement(child, { closeMenu: props.closeMenu })
-  ));
 
   beforeEach(() => {
     global.navigator = { userAgent: 'all' };
@@ -37,73 +35,107 @@ describe('Menu', () => {
     global.navigator = undefined;
   });
 
-  it('should not render a section element if the closeMenu prop is not passed', () => {
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+  it('should be an instanceOf Menu', () => {
+    const component = shallow(<Menu {...props} />);
 
-    expect(wrapper.find('section')).to.have.length(1);
+    expect(component.instance()).to.be.instanceOf(Menu);
   });
 
-  it('should not render an overlay component if the closeMenu prop is not passed', () => {
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+  it('should render root elements', () => {
+    const component = shallow(<Menu {...props} />);
 
-    expect(wrapper.find(Overlay)).to.have.length(0);
+    expect(component.find('nav')).to.have.length(1);
+    expect(component.find('section')).to.have.length(1);
+    expect(component.find(MenuItem)).to.have.length(1);
   });
 
-  it('should render a nav element', () => {
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+  it('should render overlay menu elements', () => {
+    const component = shallow(<Menu {...props} />);
 
-    expect(wrapper.find('nav')).to.have.length(1);
+    expect(component.find('section')).to.have.length(1);
+    expect(component.find(Overlay)).to.have.length(0);
+
+    component.setProps({ closeMenu: noop });
+    expect(component.find('section')).to.have.length(2);
+    expect(component.find(Overlay)).to.have.length(1);
   });
 
-  it('should not render a div element if the headerIcon prop is not passed', () => {
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+  it('should render header element', () => {
+    const component = shallow(<Menu {...props} />);
 
-    expect(wrapper.find('div')).to.have.length(0);
+    expect(component.find('h1')).to.have.length(0);
+
+    component.setProps({ header: 'Text' });
+    expect(component.find('h1')).to.have.length(1);
+    expect(component.containsMatchingElement(<h1>Text</h1>)).to.equal(true);
+
+    component.setProps({ header: <span>Node</span> });
+    expect(component.find('h1')).to.have.length(1);
+    expect(component.find('h1 > span')).to.have.length(1);
+    expect(component.find('h1').containsMatchingElement(<span>Node</span>)).to.equal(true);
   });
 
-  it('should render a div element if the headerIcon prop is passed', () => {
-    props.headerIcon = <span />;
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
-
-    expect(wrapper.containsMatchingElement(<div><span /></div>)).to.equal(true);
-    props.headerIcon = null;
-  });
-
-  it('should not render an h1 element if the header prop is not passed', () => {
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
-
-    expect(wrapper.find('h1')).to.have.length(0);
-  });
-
-  it('should render an h1 element if the header prop is passed', () => {
-    props.header = 'header';
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
-
-    expect(wrapper.containsMatchingElement(<h1>header</h1>)).to.equal(true);
-    props.header = '';
-  });
-
-  it('should render children as menuItems', () => {
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
-
-    expect(wrapper.containsMatchingElement(<p>children</p>)).to.equal(true);
-  });
-
-  it('should apply closeMenu prop to children', () => {
-    props.closeMenu = noop;
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
-    const closeMenu = wrapper.find('p').prop('closeMenu');
-
-    expect(isFunction(closeMenu)).to.equal(true);
-    props.closeMenu = null;
-  });
+  // it('should not render an overlay component if the closeMenu prop is not passed', () => {
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.find(Overlay)).to.have.length(0);
+  // });
+  //
+  // it('should render a nav element', () => {
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.find('nav')).to.have.length(1);
+  // });
+  //
+  // it('should not render a div element if the headerIcon prop is not passed', () => {
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.find('div')).to.have.length(0);
+  // });
+  //
+  // it('should render a div element if the headerIcon prop is passed', () => {
+  //   props.headerIcon = <span />;
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.containsMatchingElement(<div><span /></div>)).to.equal(true);
+  //   props.headerIcon = null;
+  // });
+  //
+  // it('should not render an h1 element if the header prop is not passed', () => {
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.find('h1')).to.have.length(0);
+  // });
+  //
+  // it('should render an h1 element if the header prop is passed', () => {
+  //   props.header = 'header';
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.containsMatchingElement(<h1>header</h1>)).to.equal(true);
+  //   props.header = '';
+  // });
+  //
+  // it('should render children as children', () => {
+  //   const component = shallow(<Menu {...props} />);
+  //
+  //   expect(component.containsMatchingElement(<p>children</p>)).to.equal(true);
+  // });
+  //
+  // it('should apply closeMenu prop to children', () => {
+  //   props.closeMenu = noop;
+  //   const component = shallow(<Menu {...props} />);
+  //   const closeMenu = component.find('p').prop('closeMenu');
+  //
+  //   expect(isFunction(closeMenu)).to.equal(true);
+  //   props.closeMenu = null;
+  // });
 
   it('should execute Overlay onClick function', () => {
     const spy = sinon.spy();
     props.closeMenu = spy;
-    const wrapper = shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    const component = shallow(<Menu {...props} />);
 
-    wrapper.find(Overlay).simulate('click');
+    component.find(Overlay).simulate('click');
     expect(spy).to.have.callCount(1);
     props.closeMenu = null;
   });
@@ -111,7 +143,7 @@ describe('Menu', () => {
   it('should get sidebar styles', () => {
     const spy = sinon.spy(getStyles, 'sidebar');
 
-    shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    shallow(<Menu {...props} />);
     expect(spy).to.have.been.calledWith(props.style);
   });
 
@@ -119,7 +151,7 @@ describe('Menu', () => {
     props.headerIcon = <span />;
     const spy = sinon.spy(getStyles, 'icon');
 
-    shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    shallow(<Menu {...props} />);
     expect(spy).to.have.been.calledWith(props.iconStyle);
     props.headerIcon = null;
   });
@@ -128,7 +160,7 @@ describe('Menu', () => {
     props.header = 'header';
     const spy = sinon.spy(getStyles, 'header');
 
-    shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    shallow(<Menu {...props} />);
     expect(spy).to.have.been.calledWith(
       props.color,
       props.headerIcon,
@@ -141,7 +173,7 @@ describe('Menu', () => {
     props.closeMenu = noop;
     const spy = sinon.spy(getStyles, 'container');
 
-    shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    shallow(<Menu {...props} />);
     expect(spy).to.have.callCount(1);
     props.closeMenu = null;
   });
@@ -150,7 +182,7 @@ describe('Menu', () => {
     props.closeMenu = noop;
     const spy = sinon.spy(getStyles, 'overlay');
 
-    shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    shallow(<Menu {...props} />);
     expect(spy).to.have.been.calledWith(props.open);
     props.closeMenu = null;
   });
@@ -159,7 +191,7 @@ describe('Menu', () => {
     props.closeMenu = noop;
     const spy = sinon.spy(getStyles, 'root');
 
-    shallow(<Menu {...props} >{menuItems}</Menu>).dive();
+    shallow(<Menu {...props} />);
     expect(spy).to.have.been.calledWith(props.open, 'left', props.style);
     props.closeMenu = null;
   });
