28ec69f7a3450bcd1e7cfa100a7b377d62710aea
Ian Stewart <ian.stewart@anchor.chat>
2018-11-19 10:29:55 +0100

    Adjusted date prop to text prop.

--- src/date-separator/component.jsx
+++ src/date-separator/component.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import getStyles from './get-styles';
 
 const propTypes = {
-  /** The date to display */
-  date: PropTypes.string.isRequired,
+  /** Separator text */
+  text: PropTypes.node.isRequired,
   /** Override the styles of the root element */
   style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   /** Override the styles of the text element */
@@ -20,10 +20,10 @@ const displayName = 'DateSeparator';
 
 /** A separator to show above a Message */
 const DateSeparator = ({
-  date, style, textStyle,
+  text, style, textStyle,
 }) => (
   <section style={getStyles.root(style)}>
-    <p style={getStyles.text(textStyle)}>{date}</p>
+    <p style={getStyles.text(textStyle)}>{text}</p>
   </section>
 );
 
